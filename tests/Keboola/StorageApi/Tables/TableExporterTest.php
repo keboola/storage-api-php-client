ed3d4364b380aecb0970adbabefc7f439d7827e7
Martin Halamíček <martin@halamicek.cz>
2014-09-08 15:02:48 -0700

    test: fixes in table exporter tests

--- tests/Keboola/StorageApi/Tables/TableExporterTest.php
+++ tests/Keboola/StorageApi/Tables/TableExporterTest.php
@@ -94,8 +94,8 @@ class Keboola_StorageApi_Tables_TableExporterTest extends StorageApiTestCase
 			array(self::BACKEND_MYSQL, new CsvFile('https://s3.amazonaws.com/keboola-tests/languages.csv.gz'), 'languages.csv'),
 			array(self::BACKEND_MYSQL, new CsvFile('https://s3.amazonaws.com/keboola-tests/languages.csv.gz'), 'languages.csv', array('gzip' => true)),
 
-			array(self::BACKEND_REDSHIFT, new CsvFile('https://s3.amazonaws.com/keboola-tests/escaping.csv'), 'escaping.escaped.out.csv', array('format' => 'escaped')),
-			array(self::BACKEND_REDSHIFT, new CsvFile('https://s3.amazonaws.com/keboola-tests/escaping.csv'), 'escaping.escaped.out.csv', array('format' => 'escaped')),
+			array(self::BACKEND_REDSHIFT, new CsvFile('https://s3.amazonaws.com/keboola-tests/escaping.csv'), 'escaping.backslash.out.csv', array('format' => 'escaped')),
+			array(self::BACKEND_REDSHIFT, new CsvFile('https://s3.amazonaws.com/keboola-tests/escaping.csv'), 'escaping.backslash.out.csv', array('format' => 'escaped')),
 			array(self::BACKEND_REDSHIFT, new CsvFile('https://s3.amazonaws.com/keboola-tests/escaping.csv'), 'escaping.raw.out.csv', array('format' => 'raw')),
 			array(self::BACKEND_REDSHIFT, new CsvFile('https://s3.amazonaws.com/keboola-tests/escaping.csv'), 'escaping.raw.out.csv', array('gzip' => true, 'format' => 'raw')),
 			array(self::BACKEND_REDSHIFT, new CsvFile('https://s3.amazonaws.com/keboola-tests/escaping.csv'), 'escaping.standard.out.csv', array('gzip' => true)),
