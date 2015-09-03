3227b4b9f4990836d9bf45aeaea90ce5ff3fe3cb
Erik Zigo <erik.zigo@keboola.com>
2015-09-03 16:24:43 +0200

    Test for primary key add - redshift

--- tests/Keboola/StorageApi/Tables/AlterTest.php
+++ tests/Keboola/StorageApi/Tables/AlterTest.php
@@ -101,7 +101,7 @@ class Keboola_StorageApi_Tables_AlterTest extends StorageApiTestCase
 		$importFile = __DIR__ . '/../_data/users.csv';
 
 		$tableId = $this->_client->createTable(
-			$this->getTestBucketId(),
+			$this->getTestBucketId(self::STAGE_IN),
 			'users',
 			new CsvFile($importFile),
 			array()
@@ -145,7 +145,7 @@ class Keboola_StorageApi_Tables_AlterTest extends StorageApiTestCase
 		$importFile = __DIR__ . '/../_data/languages-more-columns.csv';
 
 		$tableId = $this->_client->createTable(
-			$this->getTestBucketId(),
+			$this->getTestBucketId(self::STAGE_IN),
 			'languages',
 			new CsvFile($importFile),
 			array()
@@ -188,7 +188,7 @@ class Keboola_StorageApi_Tables_AlterTest extends StorageApiTestCase
 		$importFile = __DIR__ . '/../_data/languages.more-columns.csv';
 
 		$tableId = $this->_client->createTable(
-			$this->getTestBucketId(),
+			$this->getTestBucketId(self::STAGE_IN),
 			'languages-more-columns',
 			new CsvFile($importFile),
 			array(
@@ -207,6 +207,138 @@ class Keboola_StorageApi_Tables_AlterTest extends StorageApiTestCase
 		$keyCreated = false;
 		try {
 			$this->_client->createTablePrimaryKey($tableId, $primaryKeyColumns);
+			$keyCreated = true;
+		} catch (\Keboola\StorageApi\ClientException $e) {
+			if ($e->getStringCode() != 'storage.tables.cannotCreatePrimaryKey') {
+				throw $e;
+			}
+		}
+
+		$this->assertFalse($keyCreated);
+	}
+
+	public function testRedshiftPrimaryKeyAdd()
+	{
+		$primaryKeyColumns = array('id');
+		$testBucketId = $this->getTestBucketId(self::STAGE_IN, self::BACKEND_REDSHIFT);
+		$importFile = __DIR__ . '/../_data/users.csv';
+
+
+		$tableId = $this->_client->createTable(
+			$testBucketId,
+			'users',
+			new CsvFile($importFile),
+			array()
+		);
+
+		$aliasBucketId = $this->getTestBucketId(self::STAGE_OUT, self::BACKEND_REDSHIFT);
+		$sql = "SELECT * FROM \"$testBucketId\".users WHERE id='1'";
+		$aliasTableId = $this->_client->createRedshiftAliasTable($aliasBucketId, $sql, null, $tableId);
+
+		$tables = array(
+			$this->_client->getTable($tableId),
+			$this->_client->getTable($aliasTableId),
+		);
+
+		foreach ($tables AS $tableDetail) {
+			if (!$tableDetail['isAlias']) {
+				$this->assertArrayHasKey('primaryKey', $tableDetail);
+				$this->assertEmpty($tableDetail['primaryKey']);
+
+				$this->assertArrayHasKey('indexedColumns', $tableDetail);
+				$this->assertEmpty($tableDetail['indexedColumns']);
+			}
+		}
+
+		$this->_client->createTablePrimaryKey($tableId, $primaryKeyColumns);
+
+		$tables = array(
+			$this->_client->getTable($tableId),
+			$this->_client->getTable($aliasTableId),
+		);
+
+		foreach ($tables AS $tableDetail) {
+			if (!$tableDetail['isAlias']) {
+				$this->assertArrayHasKey('primaryKey', $tableDetail);
+				$this->assertEquals($primaryKeyColumns, $tableDetail['primaryKey']);
+
+				$this->assertArrayHasKey('indexedColumns', $tableDetail);
+				$this->assertEquals($primaryKeyColumns, $tableDetail['indexedColumns']);
+			}
+		}
+
+		// composite primary key
+		$primaryKeyColumns = array('Id', 'Name');
+		$testBucketId = $this->getTestBucketId(self::STAGE_IN, self::BACKEND_REDSHIFT);
+		$importFile = __DIR__ . '/../_data/languages-more-columns.csv';
+
+		$tableId = $this->_client->createTable(
+			$testBucketId,
+			'languages',
+			new CsvFile($importFile),
+			array()
+		);
+
+		$aliasBucketId = $this->getTestBucketId(self::STAGE_OUT, self::BACKEND_REDSHIFT);
+		$sql = "SELECT * FROM \"$testBucketId\".languages WHERE id='1'";
+		$aliasTableId = $this->_client->createRedshiftAliasTable($aliasBucketId, $sql, null, $tableId);
+
+		$tables = array(
+			$this->_client->getTable($tableId),
+			$this->_client->getTable($aliasTableId),
+		);
+
+		foreach ($tables AS $tableDetail) {
+			if (!$tableDetail['isAlias']) {
+				$this->assertArrayHasKey('primaryKey', $tableDetail);
+				$this->assertEmpty($tableDetail['primaryKey']);
+
+				$this->assertArrayHasKey('indexedColumns', $tableDetail);
+				$this->assertEmpty($tableDetail['indexedColumns']);
+			}
+		}
+
+		$this->_client->createTablePrimaryKey($tableId, $primaryKeyColumns);
+
+		$tables = array(
+			$this->_client->getTable($tableId),
+			$this->_client->getTable($aliasTableId),
+		);
+
+		foreach ($tables AS $tableDetail) {
+			if (!$tableDetail['isAlias']) {
+				$this->assertArrayHasKey('primaryKey', $tableDetail);
+				$this->assertEquals($primaryKeyColumns, $tableDetail['primaryKey']);
+
+				$this->assertArrayHasKey('indexedColumns', $tableDetail);
+				$this->assertEquals($primaryKeyColumns, $tableDetail['indexedColumns']);
+			}
+		}
+
+		// existing primary key
+		$primaryKeyColumn = 'id';
+		$importFile = __DIR__ . '/../_data/languages.more-columns.csv';
+
+		$tableId = $this->_client->createTable(
+			$this->getTestBucketId(self::STAGE_IN, self::BACKEND_REDSHIFT),
+			'languages-more-columns',
+			new CsvFile($importFile),
+			array(
+				'primaryKey' => $primaryKeyColumn
+			)
+		);
+
+		$tableDetail = $this->_client->getTable($tableId);
+
+		$this->assertArrayHasKey('primaryKey', $tableDetail);
+		$this->assertEquals(array($primaryKeyColumn), $tableDetail['primaryKey']);
+
+		$this->assertArrayHasKey('indexedColumns', $tableDetail);
+		$this->assertEquals(array($primaryKeyColumn), $tableDetail['indexedColumns']);
+
+		$keyCreated = false;
+		try {
+			$this->_client->createTablePrimaryKey($tableId, $primaryKeyColumn);
 			$keyCreated = true;
 		} catch (\Keboola\StorageApi\ClientException $e) {
 			if ($e->getStringCode() != 'storage.tables.cannotCreatePrimaryKey') {
