f24414ca14b5d1de3cc899510dc371d4c16da812
pivnicek <marc@keboola.com>
2016-07-12 17:38:03 +0200

    add test for perserve param

--- tests/Backend/Workspaces/WorkspacesLoadTest.php
+++ tests/Backend/Workspaces/WorkspacesLoadTest.php
@@ -34,15 +34,13 @@ class WorkspaceLoadTest extends WorkspacesTestCase
 
         $mapping1 = array("source" => $table1_id, "destination" => "languagesLoaded");
         $mapping2 = array("source" => $table2_id, "destination" => "numbersLoaded");
-        $mapping3 = array("source" => $table2_id, "destination" => "languagesLoaded");
+
 
         $input = array($mapping1, $mapping2);
 
-        $workspaces->loadWorkspaceData($workspace['id'],array("input" => $input, "preserve" => false));
+        $workspaces->loadWorkspaceData($workspace['id'],array("input" => $input));
 
         $db = $this->getDbConnection($connection);
-
-        $db->query("USE SCHEMA " . $db->quoteIdentifier($connection['schema']));
 
         $tableNames = array_map(function($table) {
             return $table['name'];
@@ -50,10 +48,36 @@ class WorkspaceLoadTest extends WorkspacesTestCase
 
         // check that the tables are in the workspace
         $tables = array_flip($tableNames);
+        $this->assertCount(2, array_keys($tables));
         $this->assertArrayHasKey("languagesLoaded", $tables);
         $this->assertArrayHasKey("numbersLoaded", $tables);
 
-        $inputDupFail = array($mapping1, $mapping3);
+        // now we'll load another table and use the preserve parameters to check that all tables are present
+        $mapping3 = array("source" => $table1_id, "destination" => "table3");
+        $workspaces->loadWorkspaceData($workspace['id'], array("input" => array($mapping3), "preserve" => true));
+
+        $tableNames = array_map(function($table) {
+            return $table['name'];
+        }, $db->fetchAll(sprintf("SHOW TABLES IN SCHEMA %s", $db->quoteIdentifier($connection["schema"]))));
+
+        $tables = array_flip($tableNames);
+        $this->assertCount(3, array_keys($tables));
+        $this->assertArrayHasKey("table3", $tables);
+        $this->assertArrayHasKey("languagesLoaded", $tables);
+        $this->assertArrayHasKey("numbersLoaded", $tables);
+
+        // now we'll try the same load, but it should clear the workspace first (preserve is false by default)
+        $workspaces->loadWorkspaceData($workspace['id'], array("input" => array($mapping3)));
+        $tableNames = array_map(function($table) {
+            return $table['name'];
+        }, $db->fetchAll(sprintf("SHOW TABLES IN SCHEMA %s", $db->quoteIdentifier($connection["schema"]))));
+        $tables = array_flip($tableNames);
+        $this->assertCount(1, array_keys($tables));
+        $this->assertArrayHasKey("table3", $tables);
+
+        // now let's try and load 2 different sources to the same destination, this request should be rejected
+        $mapping4 = array("source" => $table2_id, "destination" => "languagesLoaded");
+        $inputDupFail = array($mapping1, $mapping4);
         try {
             $workspaces->loadWorkspaceData($workspace['id'], array("input" => $inputDupFail));
             $this->fail('Attempt to write two sources to same destination should fail');
