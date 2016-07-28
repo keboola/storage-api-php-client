9385f18b81e7a9f415ccb8a9eb034a549250cbb2
pivnicek <marc@keboola.com>
2016-07-28 14:07:15 +0200

    fix: dumb switch

--- tests/Backend/Workspaces/Backend/WorkspaceBackendFactory.php
+++ tests/Backend/Workspaces/Backend/WorkspaceBackendFactory.php
@@ -6,7 +6,7 @@
  * Time: 14:03
  */
 
-namespace Keboola\Test\Backend\Workspaces;
+namespace Keboola\Test\Backend\Workspaces\Backend;
 
 use Keboola\StorageApi\Exception;
 use Keboola\Test\StorageApiTestCase;
@@ -14,11 +14,12 @@ use Keboola\Test\StorageApiTestCase;
 class WorkspaceBackendFactory
 {
     public static function createWorkspaceBackend($workspace) {
+        
         switch ($workspace['connection']['backend']) {
-            case StorageApiTestCase::BACKEND_SNOWFLAKE:
+            case StorageApiTestCase::BACKEND_REDSHIFT:
                 return new RedshiftWorkspaceBackend($workspace);
                 break;
-            case StorageApiTestCase::BACKEND_REDSHIFT:
+            case StorageApiTestCase::BACKEND_SNOWFLAKE:
                 return new SnowflakeWorkspaceBackend($workspace);
                 break;
             case StorageApiTestCase::BACKEND_MYSQL:
