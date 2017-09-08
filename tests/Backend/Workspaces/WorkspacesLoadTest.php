5f600fc60ac78342a54297e4ecc03cd5c65a294d
Erik Zigo <erik.zigo@keboola.com>
2017-09-08 13:53:09 +0200

    test: incremental load with only some columns export

--- tests/Backend/Workspaces/WorkspacesLoadTest.php
+++ tests/Backend/Workspaces/WorkspacesLoadTest.php
@@ -294,6 +294,57 @@ class WorkspaceLoadTest extends WorkspacesTestCase
         }
     }
 
+    public function testLoadIncrementalWithColumns()
+    {
+        $bucketId = $this->getTestBucketId(self::STAGE_IN);
+
+        $workspaces = new Workspaces($this->_client);
+        $workspace = $workspaces->createWorkspace();
+
+        $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
+
+        $importFile = __DIR__ . '/../../_data/languages-more-columns.csv';
+        $tableId = $this->_client->createTable(
+            $bucketId,
+            'languagesDetails',
+            new CsvFile($importFile),
+            ['primaryKey' => 'Id']
+        );
+
+        // first load
+        $options = [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languagesDetails',
+                    'whereColumn' => 'iso',
+                    'whereValues' => ['dd', 'xx'],
+                    'columns' => ['Id', 'Name'],
+                ],
+            ],
+        ];
+
+        $workspaces->loadWorkspaceData($workspace['id'], $options);
+        $this->assertEquals(2, $backend->countRows("languagesDetails"));
+
+        // second load
+        $options = [
+            'input' => [
+                [
+                    'incremental' => true,
+                    'source' => $tableId,
+                    'destination' => 'languagesDetails',
+                    'whereColumn' => 'iso',
+                    'whereValues' => ['ff', 'xx'],
+                    'columns' => ['Id', 'Name'],
+                ],
+            ],
+        ];
+
+        $workspaces->loadWorkspaceData($workspace['id'], $options);
+        $this->assertEquals(5, $backend->countRows("languagesDetails"));
+    }
+
     public function testIncrementalAdditionalColumns()
     {
         $workspaces = new Workspaces($this->_client);
