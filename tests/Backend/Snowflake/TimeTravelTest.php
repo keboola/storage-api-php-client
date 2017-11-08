23bface53d3e77a175dd9ab925b0c52cee664834
pivnicek <marc@keboola.com>
2017-11-08 17:49:35 +0100

    timetravel: rename tables

--- tests/Backend/Snowflake/TimeTravelTest.php
+++ tests/Backend/Snowflake/TimeTravelTest.php
@@ -17,29 +17,36 @@ class TimeTravelTest extends StorageApiTestCase
     public function testCreateTableFromTimestamp()
     {
         $importFile = new CsvFile(__DIR__ . '/../../_data/languages.csv');
+
+        $sourceTable = 'languages_' . date('yyyy-mm-dd_His');
+
         $sourceTableId = $this->_client->createTable(
             $this->getTestBucketId(),
-            'languages',
+            $sourceTable,
             $importFile
         );
         $originalTable = $this->_client->getTable($sourceTableId);
+        // the timestamp must be at least 1 sec > creation time
+        sleep(5);
         $timestamp = date(DATE_ATOM);
-        sleep(30);
+        sleep(25);
 
         $this->_client->writeTable($sourceTableId, $importFile, ['incremental' => true]);
 
         $updatedTable = $this->_client->getTable($sourceTableId);
 
+        $newTableName = "new-table-name_" . date('yyyy-mm-dd_His', strtotime($timestamp));
+
         $replicaTableId = $this->_client->createTableFromSourceTableAtTimestamp(
             $this->getTestBucketId(),
             $sourceTableId,
             $timestamp,
-            "new-table-name"
+            $newTableName
         );
 
         $replicaTable = $this->_client->getTable($replicaTableId);
 
-        $this->assertEquals('new-table-name', $replicaTable['name']);
+        $this->assertEquals($newTableName, $replicaTable['name']);
         $this->assertEquals($updatedTable['rowsCount'], $replicaTable['rowsCount'] * 2);
         $this->assertEquals($originalTable['rowsCount'], $replicaTable['rowsCount']);
     }
