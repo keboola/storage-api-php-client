1215b58bb07364f898e2bdcc9d1b59b57b83d097
pivnicek <marc@keboola.com>
2017-11-09 13:10:01 +0100

    test: timetravel for altered table

--- tests/Backend/Snowflake/TimeTravelTest.php
+++ tests/Backend/Snowflake/TimeTravelTest.php
@@ -61,4 +61,52 @@ class TimeTravelTest extends StorageApiTestCase
         $this->assertEquals($updatedTable['rowsCount'], $replicaTable['rowsCount'] * 2);
         $this->assertEquals($originalTable['rowsCount'], $replicaTable['rowsCount']);
     }
+
+    public function testCreateTableFromTimestampOfAlteredTable()
+    {
+        $bucketData = array(
+            'name' => 'timetravel-test',
+            'stage' => 'in',
+            'description' => 'time travel test bucket',
+        );
+        $newBucketId = $this->_client->createBucket(
+            $bucketData['name'],
+            $bucketData['stage'],
+            $bucketData['description']
+        );
+
+        $importFile = new CsvFile(__DIR__ . '/../../_data/languages.csv');
+
+        $sourceTable = 'languages_' . date('Ymd_His');
+
+        $sourceTableId = $this->_client->createTable(
+            $this->getTestBucketId(),
+            $sourceTable,
+            $importFile
+        );
+        $originalTable = $this->_client->getTable($sourceTableId);
+        // the timestamp must be at least 1 sec > creation time
+        sleep(5);
+        $timestamp = date(DATE_ATOM);
+        sleep(25);
+
+        $this->_client->addTableColumn($sourceTableId, "new-column");
+
+        $updatedTable = $this->_client->getTable($sourceTableId);
+
+        $newTableName = "new-table-name_" . date('Ymd_His', strtotime($timestamp));
+
+        $replicaTableId = $this->_client->createTableFromSourceTableAtTimestamp(
+            $newBucketId,
+            $sourceTableId,
+            $timestamp,
+            $newTableName
+        );
+
+        $replicaTable = $this->_client->getTable($replicaTableId);
+
+        $this->assertEquals($newTableName, $replicaTable['name']);
+        $this->assertEquals($originalTable['columns'], $replicaTable['columns']);
+        $this->assertGreaterThan(count($updatedTable['columns']), count($replicaTable['columns']));
+    }
 }
