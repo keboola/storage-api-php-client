7d116d2238f31c83b42d747f14648192fb9cfa51
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-11-29 15:33:40 +0100

    feat: failing testUploadSlicedFileEmptySlices test

--- tests/Common/SlicedFilesUploadTest.php
+++ tests/Common/SlicedFilesUploadTest.php
@@ -69,6 +69,65 @@ class SlicedFilesUploadTest extends StorageApiTestCase
         }
     }
 
+    public function testUploadSlicedFileEmptySlices()
+    {
+        $part1 = __DIR__ . '/../_data/sliced/empty.1';
+        $part2 = __DIR__ . '/../_data/sliced/empty.2';
+        $slices = [$part1, $part2];
+        $options = (new FileUploadOptions())
+            ->setIsSliced(true)
+            ->setFileName("empty");
+
+        $fileId = $this->_client->uploadSlicedFile($slices, $options);
+        $file = $this->_client->getFile($fileId, (new GetFileOptions())->setFederationToken(true));
+
+        $this->assertEquals($options->getIsPublic(), $file['isPublic']);
+        $this->assertEquals($options->getFileName(), $file['name']);
+        $fileSize = 0;
+        foreach ($slices as $filePath) {
+            $fileSize += filesize($filePath);
+        }
+        $this->assertEquals($fileSize, $file['sizeBytes']);
+        $manifest = json_decode(file_get_contents($file['url']), true);
+        $this->assertCount(count($slices), $manifest["entries"]);
+
+        $s3Client = new \Aws\S3\S3Client([
+            'version' => '2006-03-01',
+            'region' => $file['region'],
+            'credentials' => [
+                'key' => $file['credentials']['AccessKeyId'],
+                'secret' => $file['credentials']['SecretAccessKey'],
+                'token' => $file['credentials']['SessionToken'],
+            ]
+        ]);
+
+        foreach ($slices as $filePath) {
+            $object = $s3Client->getObject([
+                'Bucket' => $file['s3Path']['bucket'],
+                'Key' => $file["s3Path"]["key"] . basename($filePath)
+            ]);
+            $this->assertEquals(file_get_contents($filePath), $object['Body']);
+        }
+
+        $tags = $options->getTags();
+        sort($tags);
+        $fileTags = $file['tags'];
+        sort($fileTags);
+        $this->assertEquals($tags, $fileTags);
+
+        $info = $this->_client->verifyToken();
+        $this->assertEquals($file['creatorToken']['id'], (int)$info['id']);
+        $this->assertEquals($file['creatorToken']['description'], $info['description']);
+        $this->assertEquals($file['isEncrypted'], $options->getIsEncrypted());
+
+        if ($options->getIsPermanent()) {
+            $this->assertNull($file['maxAgeDays']);
+        } else {
+            $this->assertInternalType('integer', $file['maxAgeDays']);
+            $this->assertEquals(180, $file['maxAgeDays']);
+        }
+    }
+
     public function testUploadSlicedFileChunks()
     {
         $parts = 50;
