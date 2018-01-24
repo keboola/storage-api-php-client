84dd78f8aec1c8fd7985245ecddbe375dc7e3f37
Martin Halamíček <martin@halamicek.cz>
2018-01-24 11:25:11 +0100

    Tests - test time travel primary key replication

---
+++ set-env.sharing.sh
@@ -0,0 +1,4 @@
+export STORAGE_API_URL=http://192.168.64.16:8800
+export STORAGE_API_TOKEN=23479-140-17c3b089254cb80dabcc57c6c87143c591ebde3e
+export STORAGE_API_LINKING_TOKEN=23480-141-17ab7c185063dce43258e3e7855c80af0fe45cbf
+export STORAGE_API_MAINTENANCE_URL=https://maintenance-testing.us-east-1.keboola.com/
