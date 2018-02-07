9b8473145b18aaff6f20a18db765b5886b30c4ee
pivnicek <marc@keboola.com>
2018-02-07 18:05:36 +0100

    test: for setting delay method

--- tests/Common/JobPollDelayTest.php
+++ tests/Common/JobPollDelayTest.php
@@ -14,6 +14,36 @@ class JobPollDelayTest extends StorageApiTestCase
         $this->_initEmptyTestBuckets();
     }
 
+    public function testSetJobPollDelay()
+    {
+        $methodUsed = false;
+
+        $linearDelay = function ($tries) use (&$methodUsed) {
+            $methodUsed = true;
+            return (int) $tries;
+        };
+
+        $this->_client->setJobPollDelayMethoc($linearDelay);
+
+        $csvFile = new CsvFile(__DIR__ . '/../_data/languages.csv');
+        $tableId = $this->_client->createTable($this->getTestBucketId(), 'languages', $csvFile);
+
+        $fileId = $this->_client->uploadFile(
+            $csvFile->getPathname(),
+            (new \Keboola\StorageApi\Options\FileUploadOptions())
+                ->setNotify(false)
+                ->setIsPublic(false)
+                ->setCompress(false)
+                ->setTags(array('table-import'))
+        );
+        $job = $this->_client->apiPost("storage/tables/{$tableId}/import-async", [
+            'dataFileId' => $fileId
+        ], false);
+        $job = $this->_client->waitForJob($job['id']);
+
+        $this->assertTrue($methodUsed);
+    }
+
     public function testAlternateJobPollDelay()
     {
         $methodUsed = false;
@@ -30,7 +60,7 @@ class JobPollDelayTest extends StorageApiTestCase
             'maxJobPollWaitPeriodSeconds' => 20,
         ));
 
-        $csvFile = new CsvFile(__DIR__ . '/../../_data/languages.csv');
+        $csvFile = new CsvFile(__DIR__ . '/../_data/languages.csv');
         $tableId = $client->createTable($this->getTestBucketId(), 'languages', $csvFile);
 
         $fileId = $client->uploadFile(
