6bf92387ec3be6659cfcaa21798c9d01deca0d99
pivnicek <marc@keboola.com>
2018-02-14 17:18:08 +0100

    test: load to workspace at offset

--- tests/Backend/Snowflake/WorkspacesTest.php
+++ tests/Backend/Snowflake/WorkspacesTest.php
@@ -7,9 +7,12 @@
  */
 namespace Keboola\Test\Backend\Snowflake;
 
-use Keboola\Test\StorageApiTestCase;
-use Keboola\StorageApi\Workspaces;
+use Keboola\Csv\CsvFile;
 use Keboola\Db\Import\Snowflake\Connection;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\Workspaces;
+use Keboola\Test\StorageApiTestCase;
+use Keboola\Test\Backend\Workspaces\Backend\WorkspaceBackendFactory;
 
 class WorkspacesTest extends StorageApiTestCase
 {
@@ -56,5 +59,54 @@ class WorkspacesTest extends StorageApiTestCase
 
         $timeoutParam = reset($params);
         $this->assertEquals($timeoutValue, (int) $timeoutParam['value']);
+    }
+
+    public function testLoadToWorkspaceAtOffset()
+    {
+        $workspaces = new Workspaces($this->_client);
+        $workspace = $workspaces->createWorkspace();
+
+        //setup test tables
+        $sourceTableId = $this->_client->createTable(
+            $this->getTestBucketId(self::STAGE_IN),
+            'languages',
+            new CsvFile(__DIR__ . '/../../_data/languages.csv')
+        );
+        sleep(25);
+
+        $this->_client->writeTable(
+            $sourceTableId,
+            new CsvFile(__DIR__ . '/../../_data/languages.increment.csv'),
+            [
+                'incremental' => true,
+            ]
+        );
+
+        $mapping = array(
+            "source" => $sourceTableId,
+            "destination" => "offsetTestLoaded",
+            "timeOffset" => -20
+        );
+
+        $input = array($mapping);
+
+        $workspaces->loadWorkspaceData($workspace['id'], array("input" => $input));
+
+        $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
+
+        $tables = $backend->getTables();
+
+        // check that the tables are in the workspace
+        $this->assertCount(1, $tables);
+        $this->assertContains($backend->toIdentifier("offsetTestLoaded"), $tables);
+
+        // check table structure and data
+        $data = $backend->fetchAll("offsetTestLoaded", \PDO::FETCH_ASSOC);
+        $this->assertCount(2, $data[0], 'there should be two columns');
+        $this->assertArrayHasKey('id', $data[0]);
+        $this->assertArrayHasKey('name', $data[0]);
+        $this->assertCount(5, $data); // there should only be the original
+        $this->assertArrayEqualsSorted(Client::parseCsv(file_get_contents(__DIR__ . '/../../_data/languages.csv'), true, ",", '"'), $data, 'id');
+
     }
 }
