34d5568f982cffda8e9234f9d5fa3b546ae4cd96
Martin Halamíček <martin@halamicek.cz>
2018-02-21 16:34:57 +0100

    tests env templates - use local KBC

--- set-env.snowflake.template.sh
+++ set-env.snowflake.template.sh
@@ -1,3 +1,3 @@
-export STORAGE_API_URL=https://connection-testing.keboola.com/
+export STORAGE_API_URL=http://connection-apache/
 export STORAGE_API_TOKEN=
 export STORAGE_API_MAINTENANCE_URL=https://maintenance-testing.us-east-1.keboola.com/
