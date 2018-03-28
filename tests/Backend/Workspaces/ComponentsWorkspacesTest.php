f915d4af6a067d3fa554efc442b305e3492d67d7
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-28 14:49:31 +0200

    Remove excessive assertions

--- tests/Backend/Workspaces/ComponentsWorkspacesTest.php
+++ tests/Backend/Workspaces/ComponentsWorkspacesTest.php
@@ -43,15 +43,7 @@ class ComponentsWorkspacesTest extends WorkspacesTestCase
             ->setName('Main')
             ->setDescription('some desc'));
 
-        $component = $components->getConfiguration($componentId, $configurationId);
-
-        $this->assertEquals('Main', $component['name']);
-        $this->assertEquals('some desc', $component['description']);
-        $this->assertEmpty($component['configuration']);
-        $this->assertEmpty($component['changeDescription']);
-        $this->assertEquals(1, $component['version']);
-        $this->assertInternalType('int', $component['version']);
-        $this->assertInternalType('int', $component['creatorToken']['id']);
+        $components->getConfiguration($componentId, $configurationId);
 
         // create workspace
         $workspace = $components->createConfigurationWorkspace($componentId, $configurationId);
