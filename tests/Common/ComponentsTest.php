5a2b5d3d82f39b01b2d3803ac8a7e8ba55e0ede6
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-29 14:53:40 +0200

    Test does not need to assert that config is created

--- tests/Common/ComponentsTest.php
+++ tests/Common/ComponentsTest.php
@@ -195,16 +195,6 @@ class ComponentsTest extends StorageApiTestCase
             ->setName('Main')
             ->setDescription('some desc'));
 
-        $component = $components->getConfiguration($componentId, $configurationId);
-        $this->assertEquals('Main', $component['name']);
-        $this->assertEquals('some desc', $component['description']);
-        $this->assertEmpty($component['configuration']);
-        $this->assertSame('Configuration created', $component['changeDescription']);
-        $this->assertFalse($component['isDeleted']);
-        $this->assertEquals(1, $component['version']);
-        $this->assertInternalType('int', $component['version']);
-        $this->assertInternalType('int', $component['creatorToken']['id']);
-
         $components->deleteConfiguration($componentId, $configurationId);
 
         $this->assertCount(0, $components->listComponentConfigurations(
