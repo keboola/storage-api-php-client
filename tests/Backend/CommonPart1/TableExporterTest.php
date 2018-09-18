2c558a53681336eb41f47007a137db20c887ae9a
Martin Halamíček <martin@halamicek.cz>
2018-09-18 11:47:16 +0200

    refactor files base path

--- tests/Backend/CommonPart1/TableExporterTest.php
+++ tests/Backend/CommonPart1/TableExporterTest.php
@@ -86,28 +86,28 @@ class TableExporterTest extends StorageApiTestCase
 
     public function tableImportData()
     {
+        $filesBasePath = __DIR__ . '/../../_data/';
         return array(
-
-            array([self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/1200.csv'), '1200.csv'),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/languages.csv.gz'), 'languages.csv'),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/languages.encoding.csv'), 'languages.encoding.csv'),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/languages.csv.gz'), 'languages.csv', array('gzip' => true)),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/numbers.csv'), 'numbers.csv'),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/numbers.csv'), 'numbers.two-cols.csv', array('columns' => array('0', '45'))),
+            array([self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . '1200.csv'), '1200.csv'),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . 'languages.csv.gz'), 'languages.csv'),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . 'languages.encoding.csv'), 'languages.encoding.csv'),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . 'languages.csv.gz'), 'languages.csv', array('gzip' => true)),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . 'numbers.csv'), 'numbers.csv'),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . 'numbers.csv'), 'numbers.two-cols.csv', array('columns' => array('0', '45'))),
 
             // tests the redshift data too long bug https://github.com/keboola/connection/issues/412
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/languages.64k.csv'), 'languages.64k.csv'),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/languages.64k.csv'), 'languages.64k.csv',  array('gzip' => true)),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/64K.csv'), '64K.csv'),
-            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile(__DIR__ . '/../../_data/64K.csv'), '64K.csv',  array('gzip' => true)),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . 'languages.64k.csv'), 'languages.64k.csv'),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . 'languages.64k.csv'), 'languages.64k.csv',  array('gzip' => true)),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . '64K.csv'), '64K.csv'),
+            array([self::BACKEND_REDSHIFT, self::BACKEND_SNOWFLAKE], new CsvFile($filesBasePath . '64K.csv'), '64K.csv',  array('gzip' => true)),
 
-            array([self::BACKEND_REDSHIFT], new CsvFile(__DIR__ . '/../../_data/escaping.csv'), 'escaping.backslash.redshift.out.csv', array('format' => 'escaped')),
-            array([self::BACKEND_REDSHIFT], new CsvFile(__DIR__ . '/../../_data/escaping.csv'), 'escaping.backslash.redshift.out.csv', array('format' => 'escaped')),
-            array([self::BACKEND_REDSHIFT], new CsvFile(__DIR__ . '/../../_data/escaping.csv'), 'escaping.raw.redshift.out.csv', array('format' => 'raw')),
-            array([self::BACKEND_REDSHIFT], new CsvFile(__DIR__ . '/../../_data/escaping.csv'), 'escaping.raw.redshift.out.csv', array('gzip' => true, 'format' => 'raw')),
-            array([self::BACKEND_REDSHIFT], new CsvFile(__DIR__ . '/../../_data/escaping.csv'), 'escaping.standard.out.csv', array('gzip' => true)),
-            array([self::BACKEND_REDSHIFT], new CsvFile(__DIR__ . '/../../_data/numbers.csv'), 'numbers.csv', array('gzip' => true)),
-            array([self::BACKEND_REDSHIFT], new CsvFile(__DIR__ . '/../../_data/numbers.csv'), 'numbers.two-cols.csv', array('gzip' => true, 'columns' => array('0', '45'))),
+            array([self::BACKEND_REDSHIFT], new CsvFile($filesBasePath . 'escaping.csv'), 'escaping.backslash.redshift.out.csv', array('format' => 'escaped')),
+            array([self::BACKEND_REDSHIFT], new CsvFile($filesBasePath . 'escaping.csv'), 'escaping.backslash.redshift.out.csv', array('format' => 'escaped')),
+            array([self::BACKEND_REDSHIFT], new CsvFile($filesBasePath . 'escaping.csv'), 'escaping.raw.redshift.out.csv', array('format' => 'raw')),
+            array([self::BACKEND_REDSHIFT], new CsvFile($filesBasePath . 'escaping.csv'), 'escaping.raw.redshift.out.csv', array('gzip' => true, 'format' => 'raw')),
+            array([self::BACKEND_REDSHIFT], new CsvFile($filesBasePath . 'escaping.csv'), 'escaping.standard.out.csv', array('gzip' => true)),
+            array([self::BACKEND_REDSHIFT], new CsvFile($filesBasePath . 'numbers.csv'), 'numbers.csv', array('gzip' => true)),
+            array([self::BACKEND_REDSHIFT], new CsvFile($filesBasePath . 'numbers.csv'), 'numbers.two-cols.csv', array('gzip' => true, 'columns' => array('0', '45'))),
         );
     }
 }
