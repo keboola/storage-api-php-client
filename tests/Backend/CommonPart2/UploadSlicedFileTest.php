a7beea8bc9bbe76f354549fa5df208bc4c6288cc
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2018-11-19 13:16:20 +0100

    feat: add tests

---
+++ tests/Backend/CommonPart2/UploadSlicedFileTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Keboola\Test\Backend\CommonPart2;
+
+use Keboola\StorageApi\ClientException;
+use Keboola\Test\StorageApiTestCase;
+
+use Keboola\Csv\CsvFile;
+
+class UploadSlicedFileTest extends StorageApiTestCase
+{
+    public function testNoCompress()
+    {
+        $uploadOptions = new \Keboola\StorageApi\Options\FileUploadOptions();
+        $uploadOptions
+            ->setFileName('entries_')
+            ->setIsEncrypted(true)
+            ->setIsSliced(true)
+            ->setCompress(false);
+        $slices = [
+            __DIR__ . '/../../_data/sliced/neco_0000_part_00',
+        ];
+        $slicedFileId = $this->_client->uploadSlicedFile($slices, $uploadOptions);
+        $slicedFile = $this->_client->getFile($slicedFileId);
+
+        $this->assertEquals(filesize(__DIR__ . '/../../_data/sliced/neco_0000_part_00'), $slicedFile['sizeBytes']);
+    }
+
+    public function testCompress()
+    {
+        $uploadOptions = new \Keboola\StorageApi\Options\FileUploadOptions();
+        $uploadOptions
+            ->setFileName('entries_')
+            ->setIsEncrypted(true)
+            ->setIsSliced(true)
+            ->setCompress(true);
+        $slices = [
+            __DIR__ . '/../../_data/sliced/neco_0000_part_00',
+        ];
+        $slicedFileId = $this->_client->uploadSlicedFile($slices, $uploadOptions);
+        $slicedFile = $this->_client->getFile($slicedFileId);
+
+        $this->assertLessThan(filesize(__DIR__ . '/../../_data/sliced/neco_0000_part_00'), $slicedFile['sizeBytes']);
+    }
+}
