846c956f8e352868331893d9daf077613b2b3919
pivnicek <marc@keboola.com>
2018-11-22 16:25:56 +0100

    create 5 jobs in setup

--- tests/Common/JobsListTest.php
+++ tests/Common/JobsListTest.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace Keboola\Test\Common;
 
+use Keboola\Csv\CsvFile;
 use Keboola\Test\StorageApiTestCase;
 
 class JobsListTest extends StorageApiTestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_initEmptyTestBuckets();
+        // make 5 dummy jobs
+        for ($i = 0; $i < 5; $i++) {
+            $this->_client->createTableAsync(
+                $this->getTestBucketId(),
+                'dummy_job_'. $i,
+                new CsvFile(__DIR__ . '/../_data/languages.csv')
+            );
+        }
+    }
+
     public function testJobsListing(): void
     {
         $jobs = $this->_client->listJobs();
@@ -19,13 +34,11 @@ class JobsListTest extends StorageApiTestCase
         $fiveJobs = $this->_client->listJobs(["limit" => 5]);
         $this->assertLessThanOrEqual(5, count($fiveJobs));
 
-        //if there are at least 2 jobs we can check the offset parameter
-        if (count($fiveJobs) >= 2) {
-            $firstJob = $fiveJobs[0];
-            $secondJob = $fiveJobs[1];
-            $offsetJobs = $this->_client->listJobs(["limit" => 5, "offset" => 1]);
-            $this->assertNotEquals($firstJob, $offsetJobs[0]);
-            $this->assertEquals($secondJob, $offsetJobs[0]);
-        }
+        // check the offset parameter
+        $firstJob = $fiveJobs[0];
+        $secondJob = $fiveJobs[1];
+        $offsetJobs = $this->_client->listJobs(["limit" => 5, "offset" => 1]);
+        $this->assertNotEquals($firstJob, $offsetJobs[0]);
+        $this->assertEquals($secondJob, $offsetJobs[0]);
     }
 }
