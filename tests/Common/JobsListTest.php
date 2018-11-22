b552b5f8e392363685820c157f109e8b94755b8e
pivnicek <marc@keboola.com>
2018-11-22 16:30:14 +0100

    use just 2 jobs instead of 5

--- tests/Common/JobsListTest.php
+++ tests/Common/JobsListTest.php
@@ -13,8 +13,8 @@ class JobsListTest extends StorageApiTestCase
     {
         parent::setUp();
         $this->_initEmptyTestBuckets();
-        // make 5 dummy jobs
-        for ($i = 0; $i < 5; $i++) {
+        // make 2 jobs
+        for ($i = 0; $i < 2; $i++) {
             $this->_client->createTableAsync(
                 $this->getTestBucketId(),
                 'dummy_job_'. $i,
@@ -30,14 +30,14 @@ class JobsListTest extends StorageApiTestCase
         // the default limit is 20 jobs
         $this->assertLessThanOrEqual(20, count($jobs));
 
-        // get only 5 jobs
-        $fiveJobs = $this->_client->listJobs(["limit" => 5]);
-        $this->assertLessThanOrEqual(5, count($fiveJobs));
+        // get only 2 jobs
+        $twoJobs = $this->_client->listJobs(["limit" => 2]);
+        $this->assertEquals(2, count($twoJobs));
 
         // check the offset parameter
-        $firstJob = $fiveJobs[0];
-        $secondJob = $fiveJobs[1];
-        $offsetJobs = $this->_client->listJobs(["limit" => 5, "offset" => 1]);
+        $firstJob = $twoJobs[0];
+        $secondJob = $twoJobs[1];
+        $offsetJobs = $this->_client->listJobs(["limit" => 2, "offset" => 1]);
         $this->assertNotEquals($firstJob, $offsetJobs[0]);
         $this->assertEquals($secondJob, $offsetJobs[0]);
     }
