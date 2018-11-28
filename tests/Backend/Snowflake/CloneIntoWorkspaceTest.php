356c0d18df1efbdc24343ab4d9c247df39fa5534
Martin Halamíček <martin@halamicek.cz>
2018-11-28 12:01:04 +0100

    tests  - use data provider

--- tests/Backend/Snowflake/CloneIntoWorkspaceTest.php
+++ tests/Backend/Snowflake/CloneIntoWorkspaceTest.php
@@ -15,7 +15,12 @@ class CloneIntoWorkspaceTest extends WorkspacesTestCase
 {
     const IMPORT_FILE_PATH = __DIR__ . '/../../_data/languages.csv';
 
-    public function testCloneInputMapping(): void
+    /**
+     * @dataProvider cloneProvider
+     * @param bool $isSourceTableAlias
+     * @throws Exception
+     */
+    public function testClone(bool $isSourceTableAlias): void
     {
         $bucketId = $this->getTestBucketId(self::STAGE_IN);
         $tableId = $this->createTableFromFile(
@@ -24,7 +29,68 @@ class CloneIntoWorkspaceTest extends WorkspacesTestCase
             self::IMPORT_FILE_PATH
         );
 
-        $this->runAndAssertWorkspaceClone($tableId);
+        if ($isSourceTableAlias) {
+            $bucketId = $this->getTestBucketId(self::STAGE_OUT);
+            $tableId = $this->_client->createAliasTable($bucketId, $tableId);
+        }
+
+        $workspacesClient = new Workspaces($this->_client);
+
+        $workspace = $workspacesClient->createWorkspace([
+            'name' => 'clone',
+        ]);
+
+        $workspacesClient->cloneIntoWorkspace($workspace['id'], [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languagesDetails',
+                ],
+            ],
+        ]);
+
+        $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
+        $workspaceTableColumns = $backend->describeTableColumns('languagesDetails');
+        $this->assertEquals(
+            [
+                [
+                    'name' => 'id',
+                    'type' => 'VARCHAR(1048576)',
+                ],
+                [
+                    'name' => 'name',
+                    'type' => 'VARCHAR(1048576)',
+                ],
+                [
+                    'name' => '_timestamp',
+                    'type' => 'TIMESTAMP_NTZ(9)',
+                ]
+            ],
+            array_map(
+                function (array $column) {
+                    return [
+                        'name' => $column['name'],
+                        'type' => $column['type'],
+                    ];
+                },
+                $workspaceTableColumns
+            )
+        );
+
+        $workspaceTableData = $backend->fetchAll('languagesDetails');
+        $this->assertCount(5, $workspaceTableData);
+    }
+
+    public function cloneProvider()
+    {
+        return [
+          'normal table' => [
+              false,
+          ],
+          'simple alias' => [
+              true,
+          ]
+        ];
     }
 
     public function testCloneMultipleTables(): void
@@ -63,20 +129,6 @@ class CloneIntoWorkspaceTest extends WorkspacesTestCase
         $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
         $backendTables = $backend->getTables();
         $this->assertCount(2, $backendTables);
-    }
-
-    public function testCloneSimpleAlias(): void
-    {
-        $bucketId = $this->getTestBucketId(self::STAGE_IN);
-        $sourceTableId = $this->createTableFromFile(
-            $this->_client,
-            $bucketId,
-            self::IMPORT_FILE_PATH
-        );
-        $bucketId = $this->getTestBucketId(self::STAGE_OUT);
-        $tableId = $this->_client->createAliasTable($bucketId, $sourceTableId);
-
-        $this->runAndAssertWorkspaceClone($tableId);
     }
 
     /**
@@ -237,59 +289,5 @@ class CloneIntoWorkspaceTest extends WorkspacesTestCase
             new CsvFile($importFilePath),
             ['primaryKey' => $primaryKey]
         );
-    }
-
-    /**
-     * @param $tableId
-     * @param $workspacesClient
-     * @param $workspace
-     */
-    private function runAndAssertWorkspaceClone($tableId): void
-    {
-        $workspacesClient = new Workspaces($this->_client);
-
-        $workspace = $workspacesClient->createWorkspace([
-            'name' => 'clone',
-        ]);
-
-        $workspacesClient->cloneIntoWorkspace($workspace['id'], [
-            'input' => [
-                [
-                    'source' => $tableId,
-                    'destination' => 'languagesDetails',
-                ],
-            ],
-        ]);
-
-        $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
-        $workspaceTableColumns = $backend->describeTableColumns('languagesDetails');
-        $this->assertEquals(
-            [
-                [
-                    'name' => 'id',
-                    'type' => 'VARCHAR(1048576)',
-                ],
-                [
-                    'name' => 'name',
-                    'type' => 'VARCHAR(1048576)',
-                ],
-                [
-                    'name' => '_timestamp',
-                    'type' => 'TIMESTAMP_NTZ(9)',
-                ]
-            ],
-            array_map(
-                function (array $column) {
-                    return [
-                        'name' => $column['name'],
-                        'type' => $column['type'],
-                    ];
-                },
-                $workspaceTableColumns
-            )
-        );
-
-        $workspaceTableData = $backend->fetchAll('languagesDetails');
-        $this->assertCount(5, $workspaceTableData);
     }
 }
