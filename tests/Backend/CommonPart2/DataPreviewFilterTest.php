b4f106a0d5190f41b194f407bb95f79a88b902af
Vojta Tůma <ja@vojta-tuma.cz>
2019-01-21 10:06:05 +0100

    Simple test for filtering datapreview

---
+++ tests/Backend/CommonPart2/DataPreviewFilterTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Test\Backend\CommonPart2;
+
+use Keboola\Csv\CsvFile;
+use Keboola\StorageApi\Client;
+use Keboola\Test\StorageApiTestCase;
+
+class DataPreviewFilterTest extends StorageApiTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_initEmptyTestBuckets();
+    }
+
+    public function testDataPreviewWithWhereConditions()
+    {
+        $csvFile = new CsvFile(tempnam(sys_get_temp_dir(), 'keboola'));
+        $csvFile->writeRow(["column_1", "column_2", "column_3"]);
+        $csvFile->writeRow(["first",1,2]);
+        $csvFile->writeRow(["second",4,5]);
+        $tableId = $this->_client->createTable($this->getTestBucketId(), 'conditions', $csvFile);
+
+        $where = [
+            [
+                "col" => "column_1",
+                "op" => "=",
+                "val" => "first",
+            ]
+        ];
+        $preview = $this->_client->getTableDataPreview($tableId, ['where' => $where]);
+
+        $this->assertCount(1, Client::parseCsv($preview));
+    }
+}
