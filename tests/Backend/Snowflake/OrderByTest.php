f55d55aefad2f310787aa17c59dbaded50bb1b53
Vojta Tůma <ja@vojta-tuma.cz>
2019-02-13 16:22:02 +0100

    Add test for invalid orderData

--- tests/Backend/Snowflake/OrderByTest.php
+++ tests/Backend/Snowflake/OrderByTest.php
@@ -6,6 +6,7 @@ namespace Keboola\Test\Backend\Snowflake;
 
 use Keboola\Csv\CsvFile;
 use Keboola\StorageApi\Client;
+use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\TableExporter;
 use Keboola\Test\StorageApiTestCase;
 
@@ -57,14 +58,65 @@ class OrderByTest extends StorageApiTestCase
             [
                 'column' => 'column_string_number',
                 'order' => 'ASC',
-                'dataType' => 'INTEGER'
-            ]
+                'dataType' => 'INTEGER',
+            ],
         ];
 
         $dataPreview = $this->_client->getTableDataPreview($tableId, ['orderBy' => $order]);
         $exportTable = $this->getExportedTable($tableId, ['orderBy' => $order]);
         $this->assertSame('5', Client::parseCsv($dataPreview)[0]['column_string_number']);
         $this->assertSame('5', $exportTable[0]['column_string_number']);
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function testInvalidOrderByParamsShouldReturnErrorInDataPreview(array $order, string $message): void
+    {
+        $tableId = $this->prepareTable();
+
+        $this->expectException(ClientException::class);
+        $this->expectExceptionMessage($message);
+        $this->_client->getTableDataPreview($tableId, ['orderBy' => [$order]]);
+    }
+
+    /**
+     * @dataProvider invalidDataProvider
+     */
+    public function testInvalidOrderByParamsShouldReturnErrorInExport(array $order, string $message): void
+    {
+        $tableId = $this->prepareTable();
+
+        $this->expectException(ClientException::class);
+        $this->expectExceptionMessage($message);
+        $this->getExportedTable($tableId, ['orderBy' => [$order]]);
+    }
+
+    public function invalidDataProvider(): array
+    {
+        return [
+            [
+                [
+                    'column' => 'non-existing',
+                ],
+                'Can\'t filter by column non-existing, column does not exist',
+            ],
+            [
+                [
+                    'column' => 'column_string',
+                    'order' => 'non-existing'
+                ],
+                'Invalid sort order non-existing. Available orders are [DESC|ASC]',
+            ],
+            [
+                [
+                    'column' => 'column_string',
+                    'order' => 'DESC',
+                    'dataType' => 'non-existing'
+                ],
+                'Data type non-existing not recognized. Possible datatypes are [INTEGER|DOUBLE]',
+            ]
+        ];
     }
 
     private function getExportedTable(string $tableId, array $exportOptions): array
