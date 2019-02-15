10e98aa3f1f3f78ef94d0df7386580fbf176db76
Vojta Tůma <ja@vojta-tuma.cz>
2019-02-15 10:05:31 +0100

    Test for invalid filter combination

--- tests/Backend/Snowflake/FulltextSearchTest.php
+++ tests/Backend/Snowflake/FulltextSearchTest.php
@@ -6,6 +6,7 @@ namespace Keboola\Test\Backend\Snowflake;
 
 use Keboola\Csv\CsvFile;
 use Keboola\StorageApi\Client;
+use Keboola\StorageApi\ClientException;
 use Keboola\Test\StorageApiTestCase;
 
 class FulltextSearchTest extends StorageApiTestCase
@@ -24,7 +25,7 @@ class FulltextSearchTest extends StorageApiTestCase
         $dataPreviewCsv = Client::parseCsv($dataPreview);
 
         $this->assertCount(2, $dataPreviewCsv);
-        $this->assertSame('containsB', $dataPreviewCsv[0]['column_2']);
+        $this->assertSame('BcontainsBB', $dataPreviewCsv[0]['column_2']);
     }
 
     public function testFindNonExistingDataByFulltext()
@@ -37,13 +38,32 @@ class FulltextSearchTest extends StorageApiTestCase
         $this->assertCount(0, $dataPreviewCsv);
     }
 
+    public function testFulltextAndWhereFiltersAtTheSameTimeShouldFail()
+    {
+        $tableId = $this->prepareTable();
+
+        $params = [
+            'fulltextSearch' => 'ContainsA',
+            'whereFilters' => [
+                [
+                    'column' => 'column_1',
+                    'operator' => 'eq',
+                    'values' => ['AcontaionsAA']
+                ]
+            ]
+        ];
+        $this->expectException(ClientException::class);
+        $this->expectExceptionMessage('Cannot use fulltextSearch and whereFilters at the same time');
+        $this->_client->getTableDataPreview($tableId, $params);
+    }
+
     private function prepareTable(): string
     {
         $csvFile = new CsvFile(tempnam(sys_get_temp_dir(), 'keboola'));
         $csvFile->writeRow(['column_1', 'column_2', 'column_3']);
-        $csvFile->writeRow(['containsA', 'containsB', 'containsC']);
-        $csvFile->writeRow(['containsA', 'containsD', 'containsE']);
-        $csvFile->writeRow(['containsD', 'containsE', 'containsF']);
+        $csvFile->writeRow(['AcontainsAA', 'BcontainsBB', 'CcontainsCC']);
+        $csvFile->writeRow(['AcontainsAA', 'DcontainsDD', 'EcontainsEE']);
+        $csvFile->writeRow(['DcontainsDD', 'EcontainsEE', 'FcontainsFF']);
         return $this->_client->createTable($this->getTestBucketId(), 'fulltext', $csvFile);
     }
 }
