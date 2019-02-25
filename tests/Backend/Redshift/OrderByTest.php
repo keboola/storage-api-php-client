6bb5c854e6867007812ee28d4763addf34094ea8
Vojta Tůma <ja@vojta-tuma.cz>
2019-02-25 14:48:39 +0100

    Tests that orderBy is forbidden on Redshift

---
+++ tests/Backend/Redshift/OrderByTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Test\Backend\Redshift;
+
+use Keboola\Csv\CsvFile;
+use Keboola\StorageApi\ClientException;
+use Keboola\Test\StorageApiTestCase;
+
+class OrderByTest extends StorageApiTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_initEmptyTestBuckets();
+    }
+
+    public function testForbiddenWhereOperators()
+    {
+        $csvFile = new CsvFile(tempnam(sys_get_temp_dir(), 'keboola'));
+        $csvFile->writeRow(['test']);
+        $tableId = $this->_client->createTable($this->getTestBucketId(), 'conditions', $csvFile);
+
+        $this->expectException(ClientException::class);
+        $this->expectExceptionMessage('Order statements are not supported for Redshift backend.');
+        $this->_client->getTableDataPreview($tableId, ['orderBy' => [['column'=>'test']]]);
+    }
+
+}
