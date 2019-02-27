4ae4e86f41d110dc9641bea964610449923e288c
Vojta Tůma <ja@vojta-tuma.cz>
2019-02-27 13:20:12 +0100

    Add tests for forbidden fulltext search on RS backend

---
+++ tests/Backend/Redshift/FulltextSearchTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Test\Backend\Redshift;
+
+use Keboola\Csv\CsvFile;
+use Keboola\StorageApi\ClientException;
+use Keboola\Test\StorageApiTestCase;
+
+class FulltextSearchTest extends StorageApiTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_initEmptyTestBuckets();
+    }
+
+    public function testForbiddenWhereOperators()
+    {
+        $csvFile = new CsvFile(tempnam(sys_get_temp_dir(), 'keboola'));
+        $csvFile->writeRow(['test']);
+        $tableId = $this->_client->createTable($this->getTestBucketId(), 'conditions', $csvFile);
+
+        $this->expectException(ClientException::class);
+        $this->expectExceptionMessage('Fulltext search is not supported for Redshift backend.');
+        $this->_client->getTableDataPreview($tableId, ['fulltextSearch' => 'not-supported']);
+    }
+}
