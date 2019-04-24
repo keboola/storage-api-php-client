9e49e5765ad92fdc520e9a4aa57282b7fc0d7ced
Erik Zigo <erik.zigo@keboola.com>
2019-04-24 21:08:05 +0200

    fixup ExportParamsTest: removed use of legacyCreateToken method

--- tests/Backend/CommonPart1/ExportParamsTest.php
+++ tests/Backend/CommonPart1/ExportParamsTest.php
@@ -9,6 +9,8 @@
 
 namespace Keboola\Test\Backend\CommonPart1;
 
+use Keboola\StorageApi\Options\TokenCreateOptions;
+use Keboola\StorageApi\Options\TokenUpdateOptions;
 use Keboola\Test\StorageApiTestCase;
 use Keboola\StorageApi\Client;
 use Keboola\Csv\CsvFile;
@@ -204,14 +206,16 @@ class ExportParamsTest extends StorageApiTestCase
         $this->assertFalse($results['cacheHit']);
         $this->waitForFile($fileId);
 
-        $newTokenId = $this->_client->legacyCreateToken(array(
-            $this->getTestBucketId() => 'read',
-        ));
+        $tokenOptions = (new TokenCreateOptions())
+            ->addBucketPermission($this->getTestBucketId(), TokenUpdateOptions::BUCKET_PERMISSION_READ)
+        ;
+
+        $newTokenId = $this->_client->createToken($tokenOptions);
         $newToken = $this->_client->getToken($newTokenId);
-        $client = new \Keboola\StorageApi\Client(array(
+        $client = new \Keboola\StorageApi\Client([
             'token' => $newToken['token'],
             'url' => STORAGE_API_URL,
-        ));
+        ]);
 
         $results = $client->exportTableAsync($tableId);
         $this->assertTrue($results['cacheHit']);
