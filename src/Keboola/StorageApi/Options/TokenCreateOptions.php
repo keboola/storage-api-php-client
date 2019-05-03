c72ec1d59840e5b70e9128941370fea9b5126509
Erik Zigo <erik.zigo@keboola.com>
2019-05-03 15:24:16 +0200

    fixup tokenId as required parameter of TokenUpdateOptions

--- src/Keboola/StorageApi/Options/TokenCreateOptions.php
+++ src/Keboola/StorageApi/Options/TokenCreateOptions.php
@@ -2,7 +2,7 @@
 
 namespace Keboola\StorageApi\Options;
 
-class TokenCreateOptions extends TokenUpdateOptions
+class TokenCreateOptions extends TokenAbstractOptions
 {
     /** @var int|null */
     private $expiresIn;
--- src/Keboola/StorageApi/Options/TokenUpdateOptions.php
+++ src/Keboola/StorageApi/Options/TokenUpdateOptions.php
@@ -4,5 +4,19 @@ namespace Keboola\StorageApi\Options;
 
 class TokenUpdateOptions extends TokenAbstractOptions
 {
+    /** @var int $tokenId */
+    private $tokenId;
 
+    public function __construct($tokenId)
+    {
+        $this->tokenId = (int) $tokenId;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getTokenId()
+    {
+        return $this->tokenId;
+    }
 }
