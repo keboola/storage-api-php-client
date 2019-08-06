741aab2607705648ce9d3697ac511eb37ffc76e3
pivnicek <marc@keboola.com>
2019-08-06 23:27:55 +0200

    redshift full type range test

--- tests/Backend/Workspaces/MetadataFromRedshiftWorkspaceTest.php
+++ tests/Backend/Workspaces/MetadataFromRedshiftWorkspaceTest.php
@@ -15,35 +15,123 @@ class MetadataFromRedshiftWorkspaceTest extends WorkspacesTestCase
         $workspace = $workspaces->createWorkspace(["backend" => "redshift"]);
         $connection = $workspace['connection'];
         $db = $this->getDbConnection($connection);
-        $db->query("create table \"test.Languages3\" (
-                    \"Id\" varchar not null,
-                    \"Name\" varchar null
+        $db->query("create table \"test.metadata_columns\" (
+                    \"string\" varchar(16) not null default 'string',
+                    \"char\" char null,
+                    \"smallint\" smallint null,
+                    \"integer\" integer not null default 4,
+                    \"bigint\" bigint null,
+                    \"decimal\" decimal(10,3) not null default 234.123,
+                    \"real\" real null,
+                    \"double\" double precision null,
+                    \"boolean\" boolean not null default true,
+                    \"date\" date not null default current_date,
+                    \"timestamp\" timestamp not null default sysdate,
+                    \"timestamptz\" timestamptz not null default sysdate 
                 );");
         // create table from workspace
         $tableId = $this->_client->createTableAsyncDirect($this->getTestBucketId(self::STAGE_IN), array(
-            'name' => 'languages3',
+            'name' => 'metadata_columns',
             'dataWorkspaceId' => $workspace['id'],
-            'dataTableName' => 'test.Languages3',
+            'dataTableName' => 'test.metadata_columns',
         ));
-        $expectedIdMetadata = [
-            'KBC.datatype.type' => 'varchar',
+        $expectedStringMetadata = [
+            'KBC.datatype.type' => 'VARCHAR',
             'KBC.datatype.nullable' => '',
             'KBC.datatype.basetype' => 'STRING',
-            'KBC.datatype.length' => '256',
+            'KBC.datatype.length' => '16',
+            'KBC.datatype.default' => 'string',
         ];
-        $expectedNameMetadata = [
-            'KBC.datatype.type' => 'varchar',
+        $expectedCharMetadata = [
+            'KBC.datatype.type' => 'BPCHAR',
             'KBC.datatype.nullable' => '1',
             'KBC.datatype.basetype' => 'STRING',
-            'KBC.datatype.length' => '256',
+        ];
+        $expectedSmallintMetadata = [
+            'KBC.datatype.type' => 'INT2',
+            'KBC.datatype.nullable' => '1',
+            'KBC.datatype.basetype' => 'INTEGER',
+        ];
+        $expectedIntegerMetadata = [
+            'KBC.datatype.type' => 'INT4',
+            'KBC.datatype.nullable' => '',
+            'KBC.datatype.basetype' => 'INTEGER',
+            'KBC.datatype.default' => '4',
+        ];
+        $expectedBigIntMetadata = [
+            'KBC.datatype.type' => 'INT8',
+            'KBC.datatype.nullable' => '1',
+            'KBC.datatype.basetype' => 'INTEGER',
+        ];
+        $expectedDecimalMetadata = [
+            'KBC.datatype.type' => 'NUMERIC',
+            'KBC.datatype.nullable' => '',
+            'KBC.datatype.basetype' => 'NUMERIC',
+            'KBC.datatype.length' => '10,3',
+            'KBC.datatype.default' => '234.123',
+        ];
+        $expectedRealMetadata = [
+            'KBC.datatype.type' => 'FLOAT4',
+            'KBC.datatype.nullable' => '1',
+            'KBC.datatype.basetype' => 'FLOAT',
+        ];
+        $expectedDoubleMetadata = [
+            'KBC.datatype.type' => 'FLOAT8',
+            'KBC.datatype.nullable' => '1',
+            'KBC.datatype.basetype' => 'FLOAT',
+        ];
+        $expectedBooleanMetadata = [
+            'KBC.datatype.type' => 'BOOL',
+            'KBC.datatype.nullable' => '',
+            'KBC.datatype.basetype' => 'BOOLEAN',
+            'KBC.datatype.default' => 'true',
+        ];
+        $expectedDateMetadata = [
+            'KBC.datatype.type' => 'DATE',
+            'KBC.datatype.nullable' => '',
+            'KBC.datatype.basetype' => 'DATE',
+            'KBC.datatype.default' => '(\'now\'::text)::date',
+        ];
+        $expectedTimestampMetadata = [
+            'KBC.datatype.type' => 'TIMESTAMP',
+            'KBC.datatype.nullable' => '',
+            'KBC.datatype.basetype' => 'TIMESTAMP',
+            'KBC.datatype.default' => '(\'now\'::text)::timestamp without time zone',
+        ];
+        $expectedTimestamptzMetadata = [
+            'KBC.datatype.type' => 'TIMESTAMPTZ',
+            'KBC.datatype.nullable' => '',
+            'KBC.datatype.basetype' => 'TIMESTAMP',
+            'KBC.datatype.default' => '(\'now\'::text)::timestamp without time zone',
         ];
         // check that the new table has the correct metadata
         $table = $this->_client->getTable($tableId);
+
         $this->assertEquals([], $table['metadata']);
-        $this->assertArrayHasKey('id', $table['columnMetadata']);
-        $this->assertMetadata($expectedIdMetadata, $table['columnMetadata']['id']);
-        $this->assertArrayHasKey('name', $table['columnMetadata']);
-        $this->assertMetadata($expectedNameMetadata, $table['columnMetadata']['name']);
+        $this->assertArrayHasKey('string', $table['columnMetadata']);
+        $this->assertMetadata($expectedStringMetadata, $table['columnMetadata']['string']);
+        $this->assertArrayHasKey('char', $table['columnMetadata']);
+        $this->assertMetadata($expectedCharMetadata, $table['columnMetadata']['char']);
+        $this->assertArrayHasKey('smallint', $table['columnMetadata']);
+        $this->assertMetadata($expectedSmallintMetadata, $table['columnMetadata']['smallint']);
+        $this->assertArrayHasKey('integer', $table['columnMetadata']);
+        $this->assertMetadata($expectedIntegerMetadata, $table['columnMetadata']['integer']);
+        $this->assertArrayHasKey('bigint', $table['columnMetadata']);
+        $this->assertMetadata($expectedBigIntMetadata, $table['columnMetadata']['bigint']);
+        $this->assertArrayHasKey('decimal', $table['columnMetadata']);
+        $this->assertMetadata($expectedDecimalMetadata, $table['columnMetadata']['decimal']);
+        $this->assertArrayHasKey('real', $table['columnMetadata']);
+        $this->assertMetadata($expectedRealMetadata, $table['columnMetadata']['real']);
+        $this->assertArrayHasKey('double', $table['columnMetadata']);
+        $this->assertMetadata($expectedDoubleMetadata, $table['columnMetadata']['double']);
+        $this->assertArrayHasKey('boolean', $table['columnMetadata']);
+        $this->assertMetadata($expectedBooleanMetadata, $table['columnMetadata']['boolean']);
+        $this->assertArrayHasKey('date', $table['columnMetadata']);
+        $this->assertMetadata($expectedDateMetadata, $table['columnMetadata']['date']);
+        $this->assertArrayHasKey('timestamp', $table['columnMetadata']);
+        $this->assertMetadata($expectedTimestampMetadata, $table['columnMetadata']['timestamp']);
+        $this->assertArrayHasKey('timestamptz', $table['columnMetadata']);
+        $this->assertMetadata($expectedTimestamptzMetadata, $table['columnMetadata']['timestamptz']);
     }
 
     public function testCopyImport()
@@ -78,25 +166,26 @@ class MetadataFromRedshiftWorkspaceTest extends WorkspacesTestCase
         )), 'imported data comparsion');
         // check the created metadata
         $expectedIdMetadata = [
-            'KBC.datatype.type' => 'int4',
+            'KBC.datatype.type' => 'INT4',
             'KBC.datatype.nullable' => '',
-            'KBC.datatype.basetype' => 'STRING',
+            'KBC.datatype.basetype' => 'INTEGER',
         ];
         $expectedNameMetadata = [
-            'KBC.datatype.type' => 'varchar',
+            'KBC.datatype.type' => 'VARCHAR',
             'KBC.datatype.nullable' => '',
             'KBC.datatype.basetype' => 'STRING',
             'KBC.datatype.length' => '256',
-            'KBC.datatype.default' => '\'honza\'',
+            'KBC.datatype.default' => 'honza',
         ];
         $expectedUpdateMetadata = [
-            'KBC.datatype.type' => 'varchar',
+            'KBC.datatype.type' => 'VARCHAR',
             'KBC.datatype.nullable' => '1',
             'KBC.datatype.basetype' => 'STRING',
             'KBC.datatype.length' => '256',
         ];
         // check that the new table has the correct metadata
         $table = $this->_client->getTable($table['id']);
+        
         $this->assertEquals([], $table['metadata']);
         $this->assertArrayHasKey('id', $table['columnMetadata']);
         $this->assertMetadata($expectedIdMetadata, $table['columnMetadata']['id']);
@@ -104,7 +193,6 @@ class MetadataFromRedshiftWorkspaceTest extends WorkspacesTestCase
         $this->assertMetadata($expectedNameMetadata, $table['columnMetadata']['name']);
         $this->assertArrayHasKey('update', $table['columnMetadata']);
         $this->assertMetadata($expectedUpdateMetadata, $table['columnMetadata']['update']);
-
         $db->query("truncate table \"test.Languages3\"");
         $db->query("alter table \"test.Languages3\" ADD COLUMN \"new_col\" varchar(64)");
         $db->query("alter table \"test.Languages3\" RENAME TO \"test.Languages4\"");
@@ -128,11 +216,10 @@ class MetadataFromRedshiftWorkspaceTest extends WorkspacesTestCase
             'format' => 'rfc',
         )), 'new  column added');
         $expectedNewColMetadata = [
-            'KBC.datatype.type' => 'TEXT',
+            'KBC.datatype.type' => 'VARCHAR',
             'KBC.datatype.nullable' => '1',
             'KBC.datatype.basetype' => 'STRING',
             'KBC.datatype.length' => '64',
-            'KBC.datatype.default' => '',
         ];
         $table = $this->_client->getTable($table['id']);
         $this->assertEquals([], $table['metadata']);
@@ -142,6 +229,7 @@ class MetadataFromRedshiftWorkspaceTest extends WorkspacesTestCase
         $this->assertArrayHasKey("new_col", $table['columnMetadata']);
         $this->assertMetadata($expectedNewColMetadata, $table['columnMetadata']['new_col']);
     }
+
 
     private function assertMetadata($expectedKeyValues, $metadata)
     {
