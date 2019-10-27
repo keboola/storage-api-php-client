52e82457eb77edf4bec04baf9d9f0814fdcf8f98
Erik Zigo <info@spoluprace.net>
2019-10-27 14:11:31 +0100

    Fixup sharingBackendData

--- tests/Backend/Mixed/StorageApiSharingTestCase.php
+++ tests/Backend/Mixed/StorageApiSharingTestCase.php
@@ -189,7 +189,7 @@ abstract class StorageApiSharingTestCase extends StorageApiTestCase
     {
         return [
             [self::BACKEND_SNOWFLAKE],
-//            [self::BACKEND_REDSHIFT],
+            [self::BACKEND_REDSHIFT],
         ];
     }
 
