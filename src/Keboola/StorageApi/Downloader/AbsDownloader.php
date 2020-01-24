6a17ca7cf0033c700b6b77499a95b31982bcaf9e
Martin Zajíc <ja@mzajic.cz>
2020-01-24 15:22:54 +0100

    Revert "Revert "KBC-50 Add tests for import/export on azure blob storage""

---
+++ src/Keboola/StorageApi/Downloader/AbsDownloader.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Keboola\StorageApi\Downloader;
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+
+class AbsDownloader implements DownloaderInterface
+{
+    /**
+     * @var BlobRestProxy
+     */
+    private $client;
+
+    public function __construct(BlobRestProxy $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function downloadFileFromFileResponse($fileResponse, $tmpFilePath)
+    {
+        $result = $this->client->getBlob(
+            $fileResponse['absPath']['container'],
+            $fileResponse['absPath']['name']
+        );
+        file_put_contents($tmpFilePath, $result->getContentStream());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function downloadManifestEntry($fileResponse, $entry, $tmpFilePath)
+    {
+        list($protocol, $account, $container, $file) = AbsUrlParser::parseAbsUrl($entry['url']);
+
+        $filePath = $tmpFilePath . '_' . md5(str_replace('/', '_', $file));
+
+        $result = $this->client->getBlob(
+            $container,
+            $file
+        );
+        file_put_contents($filePath, $result->getContentStream());
+        return $filePath;
+    }
+}
