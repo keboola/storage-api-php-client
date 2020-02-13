2f2df686f32e1bc0068ea31a1dacbe260fff8e36
Roman Braciník <rbracinik@gmail.com>
2020-02-13 18:23:35 +0100

    add testing `outBytes` when loading to snowflake workspace job

--- tests/Backend/Workspaces/WorkspacesSnowflakeTest.php
+++ tests/Backend/Workspaces/WorkspacesSnowflakeTest.php
@@ -53,6 +53,19 @@ class WorkspacesSnowflakeTest extends WorkspacesTestCase
             ]
         ]);
 
+        $actualJobId = null;
+        foreach ($this->_client->listJobs() as $job) {
+            if ($job['operationName'] === 'workspaceLoad') {
+                if ((int) $job['operationParams']['workspaceId'] === $workspace['id']) {
+                    $actualJobId = $job['id'];
+                }
+            }
+        }
+        $job = $this->_client->waitForJob($actualJobId);
+
+        $this->assertArrayHasKey('metrics', $job);
+        $this->assertEquals(1024, $job['metrics']['outBytes']);
+
         $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
         $table = $backend->describeTableColumns('languages');
 
