8ef07fd092ce7b0ade5812e2aeb2f59add9208fd
Martin Zajic <zajca@zajca.cz>
2020-04-20 15:43:58 +0200

    init

---
+++ tests/Backend/Synapse/WorkspacesTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Keboola\Test\Backend\Synapse;
+
+class WorkspacesTest extends \Keboola\Test\Backend\Workspaces\WorkspacesTest
+{
+    public function testWorkspacePasswordReset()
+    {
+        $this->markTestSkipped('Reset password not yet implemented.');
+    }
+}
