092cb4afd38d781b71513719321c8ed19772dbda
Erik Zigo <erik.zigo@keboola.com>
2020-04-24 12:13:47 +0200

    tests(synapse): tableColumnAdd from common tests

--- tests/Backend/Synapse/AlterTableTest.php
+++ tests/Backend/Synapse/AlterTableTest.php
@@ -2,34 +2,10 @@
 
 namespace Keboola\Test\Backend\Synapse;
 
-use Keboola\Csv\CsvFile;
 use Keboola\Test\Backend\CommonPart1\AlterTableTest as CommonAlterTableTest;
 
 class AlterTableTest extends CommonAlterTableTest
 {
-    public function testTableColumnAdd()
-    {
-        $importFile = __DIR__ . '/../../_data/languages.csv';
-        $tableId = $this->_client->createTable($this->getTestBucketId(self::STAGE_IN), 'languages', new CsvFile($importFile));
-
-        $this->_client->addTableColumn($tableId, 'State');
-
-        $detail = $this->_client->getTable($tableId);
-
-        $this->assertArrayHasKey('columns', $detail);
-        $this->assertContains('State', $detail['columns']);
-        $this->assertEquals(array('id', 'name', 'State'), $detail['columns']);
-
-        $importFileWithNewCol = $importFile = __DIR__ . '/../../_data/languages.with-state.csv';
-        $this->_client->writeTable($tableId, new CsvFile($importFileWithNewCol));
-//        // @TODO not implemented yet
-//        $this->assertLinesEqualsSorted(
-//            file_get_contents($importFileWithNewCol),
-//            $this->_client->getTableDataPreview($tableId),
-//            'new column is imported'
-//        );
-    }
-
     public function testTablePkColumnDelete()
     {
         $this->markTestSkipped('Modifying PK for Synapse backend is not supported yet');
