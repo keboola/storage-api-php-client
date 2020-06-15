a3591935f6409497f4ea468e20ac6a14f725d99b
Roman Braciník <roman.bracinik@keboola.com>
2020-06-15 17:09:49 +0200

    add `MANAGED ACCESS` to assert

--- tests/Backend/Workspaces/WorkspacesSnowflakeTest.php
+++ tests/Backend/Workspaces/WorkspacesSnowflakeTest.php
@@ -188,7 +188,7 @@ class WorkspacesSnowflakeTest extends WorkspacesTestCase
         }
 
         $this->assertNotEmpty($workspaceSchema, 'schema not found');
-        $this->assertEquals('TRANSIENT', $workspaceSchema['options']);
+        $this->assertEquals('TRANSIENT, MANAGED ACCESS', $workspaceSchema['options']);
 
         $tables = $db->fetchAll("SHOW TABLES IN SCHEMA " . $db->quoteIdentifier($workspaceSchema['name']));
         $this->assertCount(2, $tables);
