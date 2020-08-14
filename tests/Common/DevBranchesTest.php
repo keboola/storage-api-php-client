c7d174ee388982709a0440499f28206dbbc2f01c
Tomáš Fejfar <tomas.fejfar@gmail.com>
2020-08-14 15:11:00 +0200

    Test with guest and non-admin token

--- tests/Common/DevBranchesTest.php
+++ tests/Common/DevBranchesTest.php
@@ -3,19 +3,28 @@
 namespace Keboola\Test\Common;
 
 use Exception;
+use Keboola\StorageApi\Client;
 use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\DevBranches;
+use Keboola\StorageApi\Options\TokenCreateOptions;
+use Keboola\Test\Helpers\ClientsProvider;
 use Keboola\Test\StorageApiTestCase;
 
 class DevBranchesTest extends StorageApiTestCase
 {
-    public function testCreateBranch()
-    {
-        $branches = new DevBranches($this->_client);
+    private static $cleanupAfterClassTokenId;
 
-        $branchName = __CLASS__ . time();
+    private static $teardownClient;
+    /**
+     * @dataProvider provideValidClients
+     */
+    public function testCreateBranch(Client $providedClient)
+    {
+        $token = $providedClient->verifyToken();
+        $branches = new DevBranches($providedClient);
+
+        $branchName = __CLASS__ . microtime();
         $branch = $branches->createBranch($branchName);
-        $token = $this->_client->verifyToken();
         $adminId = $token['admin']['id'];
         $projectId = $token['owner']['id'];
 
@@ -30,5 +39,52 @@ class DevBranchesTest extends StorageApiTestCase
         $this->assertArrayHasKey('admin', $branch);
         $this->assertSame(['id', 'name', 'email'], array_keys($branch['admin']));
         $this->assertSame($adminId, $branch['admin']['id']);
+    }
+
+    public function provideValidClients()
+    {
+        $guest = ClientsProvider::getGuestStorageApiClient();
+        return [
+            'admin' => [ClientsProvider::getClient()],
+            'guest' => [$guest]
+        ];
+    }
+
+    /**
+     * @dataProvider provideInvalidClients
+     */
+    public function testInsufficientClientCannotCreateBranch(Client $providedClient)
+    {
+        $branches = new DevBranches($providedClient);
+
+        $branchName = __CLASS__ . time();
+
+        $this->expectExceptionMessage('You don\'t have access to resource.');
+        $this->expectException(ClientException::class);
+
+        $branch = $branches->createBranch($branchName);
+    }
+
+    public function provideInvalidClients()
+    {
+        self::$teardownClient = ClientsProvider::getClient();
+        $tokenId = self::$teardownClient->createToken(new TokenCreateOptions());
+        $token = self::$teardownClient->getToken($tokenId);
+        self::$cleanupAfterClassTokenId = $token['id'];
+
+        $notAdminClient = ClientsProvider::getClientForToken($token['token']);
+
+        return [
+            'not admin' => [$notAdminClient],
+        ];
+    }
+
+    public static function tearDownAfterClass()
+    {
+        parent::tearDownAfterClass();
+        if (self::$cleanupAfterClassTokenId) {
+            self::$teardownClient->dropToken(self::$cleanupAfterClassTokenId);
+        }
+        self::$teardownClient = null;
     }
 }
