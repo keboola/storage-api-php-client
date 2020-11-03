26ae23c0c1ca68b71e232cbacb3f1e3cd8088efa
Martin Zajic <zajca@zajca.cz>
2020-11-03 11:09:15 +0100

    add tests for file workspaces

---
+++ tests/Backend/FileWorkspace/Backend/Abs.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Keboola\Test\Backend\FileWorkspace\Backend;
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Blob\Models\Blob;
+
+class Abs
+{
+    /** @var string */
+    private $connectionString;
+
+    /** @var mixed */
+    private $container;
+
+    public function __construct(array $connection)
+    {
+        $this->connectionString = $connection['connectionString'];
+        $this->container = $connection['container'];
+    }
+
+    /**
+     * @return Blob[]
+     */
+    public function listFiles()
+    {
+        $blobs = $this->getClient()->listBlobs($this->container);
+        return $blobs->getBlobs();
+    }
+
+    /**
+     * @return BlobRestProxy
+     */
+    public function getClient()
+    {
+        return BlobRestProxy::createBlobService($this->connectionString);
+    }
+
+    /**
+     * @return string
+     */
+    public function uploadTestingFile()
+    {
+        $filePath = tempnam(sys_get_temp_dir(), 'abs-file-upload');
+        file_put_contents($filePath, 'We fight for data');
+        $content = fopen($filePath, "rb");
+        $this->getClient()->createBlockBlob($this->container, basename($filePath), $content);
+
+        return basename($filePath);
+    }
+}
