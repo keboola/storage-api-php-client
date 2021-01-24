6b932c3213ea6859bf809db9590c5c79d4db564d
Erik Zigo <erik.zigo@keboola.com>
2021-01-24 23:39:51 +0100

    paratest: init token for workspace tests

--- tests/Backend/Workspaces/ParallelWorkspacesTestCase.php
+++ tests/Backend/Workspaces/ParallelWorkspacesTestCase.php
@@ -22,18 +22,10 @@ abstract class ParallelWorkspacesTestCase extends StorageApiTestCase
 
         $description = get_class($this) . '\\' . $this->getName();
 
-        $this->deleteOldTestTokensAndWorkspaces($description);
-
-        $tokenOptions = (new TokenCreateOptions())
-            ->setCanManageBuckets(true)
-            ->setDescription($description)
-        ;
-
-        $tokenId = $this->_client->createToken($tokenOptions);
-        $tokenData = $this->_client->getToken($tokenId);
+        $this->deleteOldTestWorkspaces($description);
 
         $this->workspaceSapiClient = new Client([
-            'token' => $tokenData['token'],
+            'token' => $this->initTestToken($description),
             'url' => STORAGE_API_URL,
             'backoffMaxTries' => 1,
             'jobPollRetryDelay' => function () {
@@ -42,19 +34,8 @@ abstract class ParallelWorkspacesTestCase extends StorageApiTestCase
         ]);
     }
 
-    private function deleteOldTestTokensAndWorkspaces($testName)
+    private function deleteOldTestWorkspaces($testName)
     {
-        $oldTestTokens = array_filter(
-            $this->_client->listTokens(),
-            function (array $token) use ($testName) {
-                return $token['description'] === $testName;
-            }
-        );
-
-        foreach ($oldTestTokens as $oldTestToken) {
-            $this->_client->dropToken($oldTestToken['id']);
-        }
-
         $workspaces = new Workspaces($this->_client);
 
         $oldTestWorkspaces = array_filter(
@@ -67,6 +48,37 @@ abstract class ParallelWorkspacesTestCase extends StorageApiTestCase
         foreach ($oldTestWorkspaces as $workspace) {
             $workspaces->deleteWorkspace($workspace['id']);
         }
+    }
+
+    /**
+     * @param string $testName
+     * @return string
+     */
+    private function initTestToken($testName)
+    {
+        $oldTestTokens = array_filter(
+            $this->_client->listTokens(),
+            function (array $token) use ($testName) {
+                return $token['description'] === $testName;
+            }
+        );
+
+        foreach ($oldTestTokens as $oldTestToken) {
+            if ($oldTestToken['canManageBuckets'] !== true) {
+                $this->_client->dropToken($oldTestToken['id']);
+            } else {
+                return $oldTestToken['token'];
+            }
+        }
+
+        $tokenOptions = (new TokenCreateOptions())
+            ->setCanManageBuckets(true)
+            ->setDescription($testName)
+        ;
+
+        $tokenId = $this->_client->createToken($tokenOptions);
+        $tokenData = $this->_client->getToken($tokenId);
+        return $tokenData['token'];
     }
 
     protected function getDbConnection($connection)
