546cea98cd936d496ea2dad6e4906b6d971104fd
Erik Zigo <erik.zigo@keboola.com>
2021-03-31 17:49:19 +0200

    tests: ParallelWorkspacesTestCase::recreateTestWorkspace

--- tests/Backend/Workspaces/WorkspacesTest.php
+++ tests/Backend/Workspaces/WorkspacesTest.php
@@ -27,7 +27,7 @@ class WorkspacesTest extends ParallelWorkspacesTestCase
         $this->_client->setRunId($runId);
         $this->workspaceSapiClient->setRunId($runId);
 
-        $workspace = $workspaces->createWorkspace();
+        $workspace = $this->recreateTestWorkspace();
         $connection = $workspace['connection'];
 
         $tokenInfo = $this->_client->verifyToken();
@@ -78,7 +78,7 @@ class WorkspacesTest extends ParallelWorkspacesTestCase
     {
         $workspaces = new Workspaces($this->workspaceSapiClient);
 
-        $workspace = $workspaces->createWorkspace();
+        $workspace = $this->recreateTestWorkspace();
 
         $connection = $workspace['connection'];
 
@@ -145,7 +145,7 @@ class WorkspacesTest extends ParallelWorkspacesTestCase
         $runId = $this->_client->generateRunId();
         $this->workspaceSapiClient->setRunId($runId);
 
-        $workspace = $workspaces->createWorkspace();
+        $workspace = $this->recreateTestWorkspace();
         $connection = $workspace['connection'];
 
         $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
