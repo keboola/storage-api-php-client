f18d4e7536e7b3526e042233ce078be76f7a5702
Martin Junger <martin.junger@keboola.com>
2021-06-24 07:38:25 +0200

    cleanup PoC

--- src/Keboola/StorageApi/Brotli/BrotliStream.php
+++ src/Keboola/StorageApi/Brotli/BrotliStream.php
@@ -9,14 +9,13 @@ use GuzzleHttp\Psr7\StreamWrapper;
 use Psr\Http\Message\StreamInterface;
 
 /**
- * Uses PHP's zlib.inflate filter to inflate deflate or gzipped content.
+ * Uses Brotli filter to decode content.
  *
- * This stream decorator skips the first 10 bytes of the given stream to remove
- * the gzip header, converts the provided stream to a PHP stream resource,
- * then appends the zlib.inflate filter. The stream is then converted back
- * to a Guzzle stream resource to be used as a Guzzle stream.
+ * This stream decorator converts the provided stream to a PHP stream resource,
+ * then appends the filter. The stream is then converted back
+ * to a Brotli stream resource to be used as a Guzzle stream.
  *
- * @link http://tools.ietf.org/html/rfc1952
+ * @link http://tools.ietf.org/html/rfc7932
  * @link http://php.net/manual/en/filters.compression.php
  *
  * @final
@@ -29,33 +28,11 @@ class BrotliStream implements StreamInterface
 
     public function __construct(StreamInterface $stream)
     {
-        // read the first 10 bytes, ie. gzip header
         $resource = StreamWrapper::getResource($stream);
         if (!in_array(self::FILTER_NAME, stream_get_filters())) {
             stream_filter_register(self::FILTER_NAME, BrotliStreamFilter::class);
         }
         stream_filter_append($resource, self::FILTER_NAME, STREAM_FILTER_READ);
         $this->stream = $stream->isSeekable() ? new Stream($resource) : new NoSeekStream(new Stream($resource));
-    }
-
-    /**
-     * @param StreamInterface $stream
-     * @param $header
-     *
-     * @return int
-     */
-    private function getLengthOfPossibleFilenameHeader(StreamInterface $stream, $header)
-    {
-        $filename_header_length = 0;
-
-        if (substr(bin2hex($header), 6, 2) === '08') {
-            // we have a filename, read until nil
-            $filename_header_length = 1;
-            while ($stream->read(1) !== chr(0)) {
-                $filename_header_length++;
-            }
-        }
-
-        return $filename_header_length;
     }
 }
