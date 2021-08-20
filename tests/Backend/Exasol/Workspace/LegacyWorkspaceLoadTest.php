38206caa40a19b0a37225081f1a649830fa73afa
zajca <zajca@zajca.cz>
2021-08-20 15:54:11 +0200

    add more passing tests

--- tests/Backend/Exasol/Workspace/LegacyWorkspaceLoadTest.php
+++ tests/Backend/Exasol/Workspace/LegacyWorkspaceLoadTest.php
@@ -6,21 +6,6 @@ use Keboola\Test\Backend\Workspaces\WorkspacesLoadTest;
 
 class LegacyWorkspaceLoadTest extends WorkspacesLoadTest
 {
-    public function testIncrementalAdditionalColumns()
-    {
-        $this->markTestSkipped('Needs createTablePrimaryKey');
-    }
-
-    public function testIncrementalMissingColumns()
-    {
-        $this->markTestSkipped('Needs createTablePrimaryKey');
-    }
-
-    public function testSecondsFilter()
-    {
-        $this->markTestSkipped('Needs incremental load');
-    }
-
     public function testDottedDestination()
     {
         $this->markTestSkipped('Dotted tables not supported');
--- tests/Backend/Exasol/Workspace/WorkspacesRenameLoadTest.php
+++ tests/Backend/Exasol/Workspace/WorkspacesRenameLoadTest.php
@@ -4,16 +4,6 @@ namespace Keboola\Test\Backend\Exasol\Workspace;
 
 class WorkspacesRenameLoadTest extends \Keboola\Test\Backend\Workspaces\WorkspacesRenameLoadTest
 {
-    public function testIncrementalAdditionalColumns()
-    {
-        $this->markTestSkipped('Needs createTablePrimaryKey');
-    }
-
-    public function testIncrementalMissingColumns()
-    {
-        $this->markTestSkipped('Needs createTablePrimaryKey');
-    }
-
     public function testDottedDestination()
     {
         $this->markTestSkipped('Dotted tables not supported');
