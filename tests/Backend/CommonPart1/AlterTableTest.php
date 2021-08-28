8cd3f7c980f80559ba4541fa83af664d53c157b5
Erik Zigo <info@spoluprace.net>
2021-08-28 12:35:30 +0200

    Revert "fix expected columns names after webalize"

--- tests/Backend/CommonPart1/AlterTableTest.php
+++ tests/Backend/CommonPart1/AlterTableTest.php
@@ -79,7 +79,7 @@ class AlterTableTest extends StorageApiTestCase
         return [
             [
                 '_abc-def----ghi_',
-                'abc_def_ghi_',
+                'abc_def_ghi',
             ],
             [
                 'žluťoučký    kůň',
@@ -87,7 +87,7 @@ class AlterTableTest extends StorageApiTestCase
             ],
             [
                 'lot__of_____underscores____',
-                'lot__of_____underscores____',
+                'lot__of_____underscores',
             ]
         ];
     }
