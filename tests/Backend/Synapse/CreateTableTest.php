387ae06b8cbd8be61d5598ee1f7b80d1a1bb7049
Roman Bracinik <roman.bracinik@keboola.com>
2021-09-06 16:03:32 +0200

    Add support for addColumn for typed table

--- tests/Backend/Synapse/CreateTableTest.php
+++ tests/Backend/Synapse/CreateTableTest.php
@@ -219,6 +219,78 @@ class CreateTableTest extends StorageApiTestCase
         $this->assertSame(1, count($data['rows']));
     }
 
+    public function testSourceTableColumnAddWithAutoSyncAliases()
+    {
+        $tableDefinition = [
+            'name' => 'my-new-table',
+            'primaryKeysNames' => ['id'],
+            'columns' => [
+                [
+                    'name' => 'id',
+                    'definition' => [
+                        'type' => 'INT',
+                    ],
+                ],
+                [
+                    'name' => 'column_decimal',
+                    'definition' => [
+                        'type' => 'DECIMAL',
+                        'length' => '4,3'
+                    ],
+                ],
+            ],
+            'distribution' => [
+                'type' => 'HASH',
+                'distributionColumnsNames' => ['id'],
+            ],
+        ];
+
+        $sourceTableId = $this->_client->createTableDefinition($this->getTestBucketId(self::STAGE_IN), $tableDefinition);
+
+        $firstAliasTableId = $this->_client->createAliasTable($this->getTestBucketId(self::STAGE_IN), $sourceTableId, 'table-1');
+        $secondAliasTableId = $this->_client->createAliasTable($this->getTestBucketId(self::STAGE_IN), $firstAliasTableId, 'table-2');
+
+        $this->_client->addTableColumn($sourceTableId, 'added_column', [
+            'type' => 'DECIMAL',
+            'length' => '4,3'
+        ]);
+
+        $expectedColumns = ['id', 'column_decimal', 'added_column'];
+        $this->assertEquals($expectedColumns, $this->_client->getTable($sourceTableId)['columns']);
+        $this->assertEquals($expectedColumns, $this->_client->getTable($firstAliasTableId)['columns']);
+        $this->assertEquals($expectedColumns, $this->_client->getTable($secondAliasTableId)['columns']);
+
+        // check that the new table has correct datypes in metadata
+        $metadataClient = new Metadata($this->_client);
+        $addedColumnMetadata = $metadataClient->listColumnMetadata("{$sourceTableId}.added_column");
+        // alias tables has metadata from source table
+        $firstAliasAddedColumnMetadata = $this->_client->getTable($firstAliasTableId)['sourceTable']['columnMetadata']['added_column'];
+        $secondAliasAddedColumnMetadata = $this->_client->getTable($secondAliasTableId)['sourceTable']['columnMetadata']['added_column'];
+
+        foreach ([$addedColumnMetadata, $firstAliasAddedColumnMetadata, $secondAliasAddedColumnMetadata] as $columnMetadata) {
+            $this->assertArrayEqualsExceptKeys([
+                'key' => 'KBC.datatype.type',
+                'value' => 'DECIMAL',
+                'provider' => 'storage',
+            ], $columnMetadata[0], ['id', 'timestamp']);
+            $this->assertArrayEqualsExceptKeys([
+                'key' => 'KBC.datatype.nullable',
+                'value' => '1',
+                'provider' => 'storage',
+            ], $columnMetadata[1], ['id', 'timestamp']);
+            $this->assertArrayEqualsExceptKeys([
+                'key' => 'KBC.datatype.basetype',
+                'value' => 'NUMERIC',
+                'provider' => 'storage',
+            ], $columnMetadata[2], ['id', 'timestamp']);
+            $this->assertArrayEqualsExceptKeys([
+                'key' => 'KBC.datatype.length',
+                'value' => '4,3',
+                'provider' => 'storage',
+            ], $columnMetadata[3], ['id', 'timestamp']);
+        }
+    }
+
     public function testCreateTableWithDistributionKey()
     {
         $bucketId = $this->getTestBucketId(self::STAGE_IN);
