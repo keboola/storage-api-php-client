259c4d7342086210753c958442f33d57b6ccd1e7
zajca <zajca@zajca.cz>
2021-10-22 10:52:55 +0200

    add testTableImportNullValuesOnPrimaryKey

--- tests/Backend/Exasol/ExasolImportExportCommonTest.php
+++ tests/Backend/Exasol/ExasolImportExportCommonTest.php
@@ -2,6 +2,7 @@
 
 namespace Keboola\Test\Backend\Exasol;
 
+use Keboola\StorageApi\ClientException;
 use Keboola\Test\Backend\CommonPart1\ImportExportCommonTest;
 use Keboola\Csv\CsvFile;
 
@@ -27,5 +28,20 @@ class ExasolImportExportCommonTest extends ImportExportCommonTest
                     new CsvFile(__DIR__ . '/../../_data/pk.multiple_exasol.increment.loaded.csv'),
                 ],
         ];
+    }
+
+    public function testTableImportNullValuesOnPrimaryKey()
+    {
+        $importFile = new CsvFile(__DIR__ . '/../../_data/languages.null.csv');
+        $this->expectException(ClientException::class);
+        $this->expectExceptionMessage('Load error: Constraint violation - not null (column name).');
+        $this->_client->createTableAsync(
+            $this->getTestBucketId(self::STAGE_IN),
+            'languages-null',
+            $importFile,
+            [
+                'primaryKey' => 'name',
+            ]
+        );
     }
 }
