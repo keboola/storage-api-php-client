abf566cce7e476865533252721f8cbad78d71fc5
Roman Bracinik <roman.bracinik@keboola.com>
2021-11-03 12:01:35 +0100

    remove markTestIncomplete for more tests

--- tests/Common/ComponentsTest.php
+++ tests/Common/ComponentsTest.php
@@ -832,9 +832,6 @@ class ComponentsTest extends StorageApiTestCase
     {
         /** @var Client $client */
         $client = $getClient($this);
-        if ($client instanceof BranchAwareClient) {
-            $this->markTestIncomplete('Failed asserting that a string is empty.');
-        }
 
         $config = (new \Keboola\StorageApi\Options\Components\Configuration())
             ->setComponentId('wr-db')
@@ -1034,9 +1031,6 @@ class ComponentsTest extends StorageApiTestCase
     {
         /** @var Client $client */
         $client = $getClient($this);
-        if ($client instanceof BranchAwareClient) {
-            $this->markTestIncomplete('Failed asserting that two strings are equal.');
-        }
 
         $config = (new \Keboola\StorageApi\Options\Components\Configuration())
             ->setComponentId('wr-db')
@@ -2383,9 +2377,6 @@ class ComponentsTest extends StorageApiTestCase
     {
         /** @var Client $client */
         $client = $getClient($this);
-        if ($client instanceof BranchAwareClient) {
-            $this->markTestIncomplete("Application error: Integrity constraint violation: 1062 Duplicate entry '4-671-transformation-main-test' for key 'PRIMARY'");
-        }
 
         $configuration = new \Keboola\StorageApi\Options\Components\Configuration();
         $configuration
