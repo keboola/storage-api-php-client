5e55a8ac3c883d82c50733fb49a614888b3c13dc
Martin Junger <martin.junger@keboola.com>
2022-03-30 19:00:55 +0200

    KBC-2435 TD workspace tests

---
+++ tests/Backend/Workspaces/Backend/TeradataWorkspaceBackend.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace Keboola\Test\Backend\Workspaces\Backend;
+
+use Doctrine\DBAL\Connection;
+use Keboola\Datatype\Definition\Teradata;
+use Keboola\StorageApi\Exception;
+use Keboola\TableBackendUtils\Column\ColumnCollection;
+use Keboola\TableBackendUtils\Column\Teradata\TeradataColumn;
+use Keboola\TableBackendUtils\Escaping\Teradata\TeradataQuote;
+use Keboola\TableBackendUtils\Schema\Teradata\TeradataSchemaReflection;
+use Keboola\TableBackendUtils\Table\Teradata\TeradataTableQueryBuilder;
+use Keboola\TableBackendUtils\Table\Teradata\TeradataTableReflection;
+use Keboola\Test\Backend\WorkspaceConnectionTrait;
+
+class TeradataWorkspaceBackend implements WorkspaceBackend
+{
+    use WorkspaceConnectionTrait;
+
+    /** @var Connection */
+    private $db;
+
+    /** @var string */
+    private $schema;
+
+    /**
+     * @param array $workspace
+     */
+    public function __construct($workspace)
+    {
+        $this->db = $this->getDbConnection($workspace['connection']);
+        $this->schema = $workspace['connection']['schema'];
+    }
+
+    /**
+     * @param string $table
+     * @return array of column names
+     */
+    public function getTableColumns($table)
+    {
+        $ref = new TeradataTableReflection($this->db, $this->schema, $table);
+        return $ref->getColumnsNames();
+    }
+
+    /**
+     * @return array of table names
+     */
+    public function getTables()
+    {
+        $ref = new TeradataSchemaReflection($this->db, $this->schema);
+        return $ref->getTablesNames();
+    }
+
+    /**
+     * @param string $table
+     * @return void
+     */
+    public function dropTable($table)
+    {
+        $qb = new TeradataTableQueryBuilder();
+        $this->db->executeStatement($qb->getDropTableCommand($this->schema, $table));
+    }
+
+    /**
+     * @param $table
+     * @param $column
+     * @return void
+     */
+    public function dropTableColumn($table, $column)
+    {
+        throw new Exception('TODO Not implemented yet');
+        /*$this->db->executeStatement(sprintf(
+            "ALTER TABLE %s.%s DROP COLUMN %s;",
+            TeradataQuote::quoteSingleIdentifier($this->schema),
+            TeradataQuote::quoteSingleIdentifier($table),
+            TeradataQuote::quoteSingleIdentifier($column)
+        ));*/
+    }
+
+    /**
+     * @param string $table
+     * @return int
+     */
+    public function countRows($table)
+    {
+        $ref = new TeradataTableReflection($this->db, $this->schema, $table);
+        return $ref->getRowsCount();
+    }
+
+    /**
+     * @param string $tableName
+     * @param array $columns
+     * @return void
+     */
+    public function createTable($tableName, $columns)
+    {
+        $cols = [];
+        /**
+         * @var string $column
+         * @var string $dataType
+         */
+        foreach ($columns as $column => $dataType) {
+            $cols[] = new TeradataColumn($column, new Teradata($dataType));
+        }
+
+        $qb = new TeradataTableQueryBuilder();
+        $this->db->executeStatement($qb->getCreateTableCommand(
+            $this->schema,
+            $tableName,
+            new ColumnCollection($cols)
+        ));
+    }
+
+    /**
+     * @param string $table
+     * @param int $style
+     * @param string $orderBy
+     * @return array
+     */
+    public function fetchAll($table, $style = \PDO::FETCH_NUM, $orderBy = null)
+    {
+        $data = [];
+        $res = $this->db->fetchAll(sprintf(
+            "SELECT * FROM %s.%s %s;",
+            TeradataQuote::quoteSingleIdentifier($this->schema),
+            TeradataQuote::quoteSingleIdentifier($table),
+            $orderBy !== null ? "ORDER BY $orderBy" : null
+        ));
+        switch ($style) {
+            case \PDO::FETCH_NUM:
+                foreach ($res as $row) {
+                    $data[] = array_values($row);
+                }
+                break;
+            case \PDO::FETCH_ASSOC:
+                $data = $res;
+                break;
+            default:
+                throw new \Exception("Unknown fetch style $style");
+                break;
+        }
+        return $data;
+    }
+
+    public function toIdentifier($item)
+    {
+        return $item;
+    }
+
+    /**
+     * @param string $tableName
+     * @return ColumnCollection
+     */
+    public function describeTableColumns($tableName)
+    {
+        $ref = new TeradataTableReflection($this->db, $this->schema, $tableName);
+        return $ref->getColumnsDefinitions();
+    }
+
+    /**
+     * @param string $tableName
+     * @return TeradataTableReflection
+     */
+    public function getTableReflection($tableName)
+    {
+        return new TeradataTableReflection($this->db, $this->schema, $tableName);
+    }
+
+    /**
+     * @param string $tableName
+     */
+    public function getViewReflection($tableName)
+    {
+        throw new Exception('TODO Not implemented yet');
+    }
+
+    /**
+     * @return TeradataSchemaReflection
+     */
+    public function getSchemaReflection()
+    {
+        return new TeradataSchemaReflection($this->db, $this->schema);
+    }
+
+    /**
+     * @return void
+     */
+    public function disconnect()
+    {
+        $this->db->close();
+    }
+
+    /**
+     * @param string $table
+     * @return void
+     */
+    public function dropTableIfExists($table)
+    {
+        if ($this->isTableExists($this->schema, $table)) {
+            $this->db->executeStatement(sprintf(
+                "DROP TABLE %s.%s;",
+                TeradataQuote::quoteSingleIdentifier($this->schema),
+                TeradataQuote::quoteSingleIdentifier($table)
+            ));
+        }
+    }
+
+    /**
+     * @param string $databaseName
+     * @param string $tableName
+     * @return bool
+     */
+    protected function isTableExists($databaseName, $tableName)
+    {
+        $tables = $this->db->fetchAllAssociative(sprintf(
+            'SELECT TableName FROM DBC.TablesVX WHERE DatabaseName = %s AND TableName = %s',
+            TeradataQuote::quote($databaseName),
+            TeradataQuote::quote($tableName)
+        ));
+        return count($tables) === 1;
+    }
+}
