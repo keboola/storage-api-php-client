6ad9028b1ebc28f582d58f94b667a4e411b4f453
Tomáš Fejfar <tomas.fejfar@gmail.com>
2022-04-12 13:43:41 +0200

    KBC-2540 Dynamic timeout for different endpoints

---
+++ src/Keboola/StorageApi/Client/RequestTimeoutMiddleware.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Keboola\StorageApi\Client;
+
+use Keboola\StorageApi\Client;
+use Psr\Http\Message\RequestInterface;
+
+class RequestTimeoutMiddleware
+{
+    const REQUEST_TIMEOUT_DEFAULT = 60;
+    const REQUEST_TIMEOUT_EXTENDED = 7200;
+
+    /** @var callable */
+    private $nextHandler;
+
+    private function __construct(
+        callable $nextHandler
+    ) {
+        $this->nextHandler = $nextHandler;
+    }
+
+    /**
+     * @return callable
+     */
+    public static function factory()
+    {
+        return function (callable $handler) {
+            return new self($handler);
+        };
+    }
+
+    /**
+     * @return mixed
+     */
+    public function __invoke(
+        RequestInterface $request,
+        array $options
+    ) {
+        $nextHander = $this->nextHandler;
+
+        $isExtendedTimeout = array_key_exists(Client::REQUEST_OPTION_EXTENDED_TIMEOUT, $options) ? (bool) $options[Client::REQUEST_OPTION_EXTENDED_TIMEOUT] : false;
+        if ($request->getMethod() === 'DELETE') {
+            $isExtendedTimeout = true;
+        }
+
+        $options['timeout'] = self::REQUEST_TIMEOUT_DEFAULT;
+        if ($isExtendedTimeout) {
+            $options['timeout'] = self::REQUEST_TIMEOUT_EXTENDED;
+        }
+        return $nextHander($request, $options);
+    }
+}
---
+++ tests-unit/Client/RequestTimeoutMiddlewareTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Keboola\UnitTest\Client;
+
+use GuzzleHttp\Psr7\Request;
+use Keboola\StorageApi\Client;
+use Keboola\StorageApi\Client\RequestTimeoutMiddleware;
+use PHPUnit\Framework\TestCase;
+
+class RequestTimeoutMiddlewareTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testWillSetDefaultTimeout()
+    {
+        $assertingHandler = function ($request, $options) {
+            $this->assertSame(60, $options['timeout']);
+        };
+        $requestMock = new Request('GET', '/lorem-ipsum');
+
+        $middleware = RequestTimeoutMiddleware::factory()($assertingHandler);
+
+        $middleware($requestMock, []);
+    }
+
+    /**
+     * @return void
+     */
+    public function testWillOverrideTimeout()
+    {
+        $assertingHandler = function ($request, $options) {
+            $this->assertSame(60, $options['timeout']);
+        };
+        $requestMock = new Request('GET', '/lorem-ipsum');
+
+        $middleware = RequestTimeoutMiddleware::factory()($assertingHandler);
+
+        $middleware($requestMock, ['timeout' => 300]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testWillSetDeleteTimeout()
+    {
+        $assertingHandler = function ($request, $options) {
+            $this->assertSame(7200, $options['timeout']);
+        };
+        $requestMock = new Request('DELETE', '/lorem-ipsum');
+
+        $middleware = RequestTimeoutMiddleware::factory()($assertingHandler);
+
+        $middleware($requestMock, []);
+    }
+
+    /**
+     * @return void
+     */
+    public function testWillSetManualExtenededTimeout()
+    {
+        $assertingHandler = function ($request, $options) {
+            $this->assertSame(7200, $options['timeout']);
+        };
+        $requestMock = new Request('DELETE', '/lorem-ipsum');
+
+        $middleware = RequestTimeoutMiddleware::factory()($assertingHandler);
+
+        $middleware($requestMock, [Client::REQUEST_OPTION_EXTENDED_TIMEOUT => true, 'timeout' => 123]);
+    }
+}
