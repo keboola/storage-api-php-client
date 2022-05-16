ed7d8520199594e174dc8896b0d660a31ec3e106
Tomáš Fejfar <tomas.fejfar@gmail.com>
2022-05-16 10:00:20 +0200

    Init Rector

---
+++ rector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/tests-unit',
+    ]);
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    $rectorConfig->sets([
+        PHPUnitSetList::PHPUNIT_70,
+    ]);
+};
