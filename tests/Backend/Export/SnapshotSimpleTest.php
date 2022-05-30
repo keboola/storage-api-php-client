482763ac3c4160155809515f22abf79ee4d5ab7b
Martin Junger <martin.junger@keboola.com>
2022-05-30 17:46:41 +0200

    KBC-2647 add simple test to create table from snapshot

--- tests/Backend/Export/SnapshotSimpleTest.php
+++ tests/Backend/Export/SnapshotSimpleTest.php
@@ -37,4 +37,31 @@ class SnapshotSimpleTest extends StorageApiTestCase
         $this->assertArrayHasKey('creatorToken', $snapshot);
         $this->assertNotEmpty($snapshot['dataFileId']);
     }
+
+    /**
+     * Copy of SnapshottingTest::testCreateTableFromSnapshotWithDifferentName
+     * @see \Keboola\Test\Backend\CommonPart2\SnapshottingTest::testCreateTableFromSnapshotWithDifferentName
+     */
+    public function testCreateTableFromSnapshot(): void
+    {
+        // create snapshot
+        $importFile = __DIR__ . '/../../_data/users.csv';
+        $csvFile = new CsvFile($importFile);
+        $tableId = $this->_client->createTableAsync($this->getTestBucketId(self::STAGE_IN), 'users', $csvFile, [
+            'columns' => $csvFile->getHeader(),
+        ]);
+        $sourceTable = $this->_client->getTable($tableId);
+
+        $description = 'Test snapshot';
+        $snapshotId = $this->_client->createTableSnapshot($tableId, $description);
+
+        // create table from snapshot
+        $newTableId = $this->_client->createTableFromSnapshot($this->getTestBucketId(), $snapshotId, 'new-users');
+        $newTable = $this->_client->getTable($newTableId);
+
+        $this->assertEquals('new-users', $newTable['name']);
+
+        $this->assertSame($sourceTable['rowsCount'], $newTable['rowsCount']);
+        $this->assertSame($sourceTable['columns'], $newTable['columns']);
+    }
 }
