76dabafcc1c3020ee6376d8f8f3d1a01a1287685
zajca <zajca@zajca.cz>
2022-08-25 15:09:37 +0200

    update testUnloadFromWSToTypedTable

--- tests/Backend/Snowflake/TypedTableInWorkspaceTest.php
+++ tests/Backend/Snowflake/TypedTableInWorkspaceTest.php
@@ -2,7 +2,7 @@
 
 namespace Keboola\Test\Backend\Snowflake;
 
-use Keboola\Test\Backend\WorkspaceConnectionTrait;
+use Keboola\StorageApi\ClientException;
 use Keboola\Test\Backend\Workspaces\Backend\WorkspaceBackendFactory;
 use Keboola\Test\Backend\Workspaces\ParallelWorkspacesTestCase;
 
@@ -55,86 +55,7 @@ class TypedTableInWorkspaceTest extends ParallelWorkspacesTestCase
         return $this->_client->createTableDefinition($bucketId, $data);
     }
 
-    /**
-     * @return void
-     */
-    public function testUnloadFromWSToTypedTable()
-    {
-        // create workspace and source table in workspace
-        $workspace = $this->initTestWorkspace();
-
-        $tableId = 'Languages3';
-
-        $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
-        $backend->dropTableIfExists($tableId);
-
-        $connection = $workspace['connection'];
-
-        $db = $backend->getDb();
-
-        $quotedTableId = sprintf(
-            '"%s"."%s"',
-            $connection['schema'],
-            $tableId
-        );
-
-        $sql = sprintf(
-            '
-            CREATE TABLE %s (
-                "id" INT NOT NULL,
-                "name" VARCHAR(5000)
-            )
-        ',
-            $quotedTableId,
-        );
-        $db->query($sql);
-        $db->query(sprintf("INSERT INTO %s VALUES (1, 'john');", $quotedTableId));
-        $db->query(sprintf("INSERT INTO %s VALUES (2, 'does');", $quotedTableId));
-
-        // __temp_ table should be typed
-        $this->_client->writeTableAsyncDirect($this->tableId, [
-            'dataWorkspaceId' => $workspace['id'],
-            'dataTableName' => $tableId,
-        ]);
-
-        $expected = [
-            [
-                [
-                    'columnName' => 'id',
-                    'value' => '1',
-                    'isTruncated' => false,
-                ],
-                [
-                    'columnName' => 'name',
-                    'value' => 'john',
-                    'isTruncated' => false,
-                ],
-            ],
-            [
-                [
-                    'columnName' => 'id',
-                    'value' => '2',
-                    'isTruncated' => false,
-                ],
-                [
-                    'columnName' => 'name',
-                    'value' => 'does',
-                    'isTruncated' => false,
-                ],
-            ],
-        ];
-
-        /** @var array $data */
-        $data = $this->_client->getTableDataPreview($this->tableId, [
-            'format' => 'json',
-        ]);
-        self::assertEquals($expected, $data['rows']);
-    }
-
-    /**
-     * @return void
-     */
-    public function testUnloadFromWorkspaceToTypedTableWithInvalidTypes()
+    public function testUnloadFromWSToTypedTable(): void
     {
         // create workspace and source table in workspace
         $workspace = $this->initTestWorkspace();
@@ -158,7 +79,7 @@ class TypedTableInWorkspaceTest extends ParallelWorkspacesTestCase
             '
             CREATE TABLE %s (
                 "id" FLOAT NOT NULL,
-                "name" TEXT
+                "name" VARCHAR(5000)
             )
         ',
             $quotedTableId,
@@ -167,7 +88,32 @@ class TypedTableInWorkspaceTest extends ParallelWorkspacesTestCase
         $db->query(sprintf("INSERT INTO %s VALUES (1, 'john');", $quotedTableId));
         $db->query(sprintf("INSERT INTO %s VALUES (2, 'does');", $quotedTableId));
 
-        // __temp_ table should be typed
+        try {
+            // __temp_ table should be typed
+            $this->_client->writeTableAsyncDirect($this->tableId, [
+                'dataWorkspaceId' => $workspace['id'],
+                'dataTableName' => $tableId,
+            ]);
+            $this->fail('Should fail since createTableDefinition method creates table with VARCHAR type with no length');
+        } catch (ClientException $e) {
+            $this->assertSame('Table import error: Source destination columns mismatch. "id FLOAT NOT NULL"->"id NUMBER (38,0)"', $e->getMessage());
+        }
+
+        $db->query(sprintf('DROP TABLE %s', $quotedTableId));
+        $sql = sprintf(
+            '
+            CREATE TABLE %s (
+                "id" INT NOT NULL,
+                "name" VARCHAR(16777216)
+            )
+        ',
+            $quotedTableId,
+        );
+        $db->query($sql);
+        $db->query(sprintf("INSERT INTO %s VALUES (1, 'john');", $quotedTableId));
+        $db->query(sprintf("INSERT INTO %s VALUES (2, 'does');", $quotedTableId));
+
+        // should be OK tables types are matching
         $this->_client->writeTableAsyncDirect($this->tableId, [
             'dataWorkspaceId' => $workspace['id'],
             'dataTableName' => $tableId,
