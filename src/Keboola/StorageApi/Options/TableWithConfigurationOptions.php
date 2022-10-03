1b6ad4fa486c38e4dcf60766189d796947c30a8a
Tomáš Fejfar <tomas.fejfar@gmail.com>
2022-10-03 10:06:32 +0200

    Option object has required params in constructor

--- src/Keboola/StorageApi/Options/TableWithConfigurationOptions.php
+++ src/Keboola/StorageApi/Options/TableWithConfigurationOptions.php
@@ -5,28 +5,24 @@ namespace Keboola\StorageApi\Options;
 class TableWithConfigurationOptions
 {
     private string $tableName;
+
     private string $configurationId;
+
+    public function __construct(
+        string $tableName,
+        string $configurationId
+    ) {
+        $this->tableName = $tableName;
+        $this->configurationId = $configurationId;
+    }
 
     public function getTableName(): string
     {
         return $this->tableName;
     }
 
-
-    public function setTableName(string $tableName): self
-    {
-        $this->tableName = $tableName;
-        return $this;
-    }
-
     public function getConfigurationId(): string
     {
         return $this->configurationId;
-    }
-
-    public function setConfigurationId(string $configurationId): self
-    {
-        $this->configurationId = $configurationId;
-        return $this;
     }
 }
