e8a99ccc7e7c5f6ba99548202a54ecd8a17d32f9
Jiri Semmler <jirka.semmler@gmail.com>
2022-11-07 17:10:13 +0100

    Clean the test

--- tests/Backend/MixedSnowflakeTeradata/SharingTest.php
+++ tests/Backend/MixedSnowflakeTeradata/SharingTest.php
@@ -3,7 +3,6 @@
 namespace Keboola\Test\Backend\MixedSnowflakeTeradata;
 
 use Keboola\Csv\CsvFile;
-use Keboola\StorageApi\Client;
 use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Workspaces;
 use Keboola\TableBackendUtils\Escaping\Teradata\TeradataQuote;
@@ -15,37 +14,6 @@ class SharingTest extends StorageApiSharingTestCase
 {
     use WorkspaceConnectionTrait;
 
-    /**
-     * @return array[]
-     */
-    public function sharingBackendData()
-    {
-        return [
-            [self::BACKEND_SNOWFLAKE],
-            [self::BACKEND_TERADATA],
-        ];
-    }
-
-    /**
-     * @return array[]
-     */
-    public function workspaceMixedBackendData()
-    {
-        return [
-//            'td to snowflake' =>
-//                [
-//                    'sharing backend' => self::BACKEND_SNOWFLAKE,
-//                    'workspace backend' => self::BACKEND_TERADATA,
-//                    'load type' => 'staging',
-//                ],
-            'td to td' =>
-                [
-                    'sharing backend' => self::BACKEND_TERADATA,
-                    'workspace backend' => self::BACKEND_TERADATA,
-                    'load type' => 'direct',
-                ],
-        ];
-    }
 
     /**
      * @return void
@@ -57,76 +25,85 @@ class SharingTest extends StorageApiSharingTestCase
     }
 
     /**
-     * @dataProvider workspaceMixedBackendData
-     *
-     * @param string $sharingBackend
-     * @param string $workspaceBackend
-     * @param string $expectedLoadType
      * @return void
      * @throws ClientException
      * @throws \Doctrine\DBAL\Exception
      * @throws \Keboola\StorageApi\Exception
      */
-    public function testWorkspaceLoadData(
-        $sharingBackend,
-        $workspaceBackend,
-        $expectedLoadType
-    ): void
+    public function testAccessDataInLinkedBucketFromWSViaRO(): void
     {
         //setup test tables
         $this->deleteAllWorkspaces();
-        $this->initTestBuckets($sharingBackend);
+        $this->initTestBuckets(self::BACKEND_TERADATA);
         $bucketId = $this->getTestBucketId(self::STAGE_IN);
-        $bucketDetail = $this->_client->getBucket($bucketId);
-        $secondBucketId = $this->getTestBucketId(self::STAGE_OUT);
 
-        $table1Id = $this->_client->createTableAsync(
+        // create table in SHARING bucket in project A
+        $this->_client->createTableAsync(
             $bucketId,
             'languagesFromClient1',
             new CsvFile(__DIR__ . '/../../_data/languages.csv'),
             []
         );
 
-
-        // share and link bucket
+        // share and link bucket A->B
         $this->_client->shareBucket($bucketId);
         self::assertTrue($this->_client->isSharedBucket($bucketId));
         $response = $this->_client2->listSharedBuckets();
         self::assertCount(1, $response);
         $sharedBucket = reset($response);
-        /** @var string $linkedId */
-        $linkedId = $this->_client2->linkBucket(
+
+        $this->_client2->linkBucket(
             'linked-' . time(),
             'out',
             $sharedBucket['project']['id'],
             $sharedBucket['id']
         );
+
+        // create WS in project B
         $workspaces = new Workspaces($this->_client2);
         $workspace = $workspaces->createWorkspace(
-            [
-                'backend' => $workspaceBackend,
-            ],
+            ['backend' => self::BACKEND_TERADATA],
             true
         );
 
-        $wsConnection = $workspace['connection'];
-        $wsDb = $this->getDbConnection($wsConnection);
+        $wsDb = $this->getDbConnection($workspace['connection']);
         self::assertInstanceOf(\Doctrine\DBAL\Connection::class, $wsDb);
 
         $wsBackend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
-        $sharedBucketDbNameSuffix = $sharedBucket['project']['id'] . '-' . str_replace('.', '_', $sharedBucket['id']);
 
-
-        $data = $wsDb->fetchAllAssociative(sprintf('SELECT * FROM DBC.DatabasesVX WHERE DatabaseName LIKE %s', TeradataQuote::quote('%' . $sharedBucketDbNameSuffix)));
-        $this->assertCount(1, $data);
-        $sourceBucketDBName = $data[0]['DatabaseName'];
+        // create table in WS and insert some data
         $wsBackend->createTable('tableInWs', ['NAME' => 'VARCHAR']);
-        $wsBackend->executeQuery("INSERT INTO `tableInWs` ('pat', 'mat')");
+        $wsBackend->executeQuery(sprintf("INSERT INTO %s VALUES ('pat')", TeradataQuote::quoteSingleIdentifier('tableInWs')));
+        $wsBackend->executeQuery(sprintf("INSERT INTO %s VALUES ('mat')", TeradataQuote::quoteSingleIdentifier('tableInWs')));
 
-        // check that the tables are in the workspace
-        $dataClient1 = $wsDb->fetchAllAssociative(sprintf('SELECT * FROM %s.%s', TeradataQuote::quoteSingleIdentifier($sourceBucketDBName), TeradataQuote::quoteSingleIdentifier('languagesFromClient1')));
-        $dataClient2 = $wsBackend->fetchAll('tableInWs');
+        // try to guess name of TD DB of bucket1 in projectA. The structure of DB is <stackPrefix><projectID>-<stage>_<bucketID>
+        $sharedBucketDbNameSuffix = sprintf(
+            '%%%s-%s',
+            $sharedBucket['project']['id'],
+            str_replace('.', '_', $sharedBucket['id'])
+        );
 
-        $a = '1';
+        $foundDatabases = $wsDb->fetchAllAssociative(
+            sprintf(
+                'SELECT * FROM DBC.DatabasesVX WHERE DatabaseName LIKE %s',
+                TeradataQuote::quote($sharedBucketDbNameSuffix)
+            )
+        );
+        $this->assertCount(1, $foundDatabases);
+        $sourceBucketDBName = $foundDatabases[0]['DatabaseName'];
+        assert(is_string($sourceBucketDBName));
+
+        // check that both tables are available in the workspace
+        $dataFromLinkedTable = $wsDb->fetchAllAssociative(
+            sprintf(
+                'SELECT * FROM %s.%s',
+                TeradataQuote::quoteSingleIdentifier($sourceBucketDBName),
+                TeradataQuote::quoteSingleIdentifier('languagesFromClient1')
+            )
+        );
+        $dataCreatedInWs = $wsBackend->fetchAll('tableInWs');
+
+        $this->assertCount(2, $dataCreatedInWs);
+        $this->assertCount(5, $dataFromLinkedTable);
     }
 }
