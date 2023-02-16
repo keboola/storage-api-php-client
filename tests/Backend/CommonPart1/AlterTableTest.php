1ce51b8f1ac2f4dbfc7d57c61b6e857ecf3b4ee6
Martin Junger <martin.junger@keboola.com>
2023-02-16 17:38:36 +0100

    move test with tables-definition to CommonPart2
    add skip for Redshift

--- tests/Backend/CommonPart1/AlterTableTest.php
+++ tests/Backend/CommonPart1/AlterTableTest.php
@@ -210,37 +210,6 @@ class AlterTableTest extends StorageApiTestCase
         }
     }
 
-    public function testTooManyColumnsCountFailed(): void
-    {
-        $this->skipTestForBackend([
-            self::BACKEND_EXASOL,
-            self::BACKEND_BIGQUERY,
-        ], 'Backend has no limit regarding number of columns');
-
-        $data = [
-            'name' => 'tooManyColumns',
-            'primaryKeysNames' => [],
-            'columns' => [],
-        ];
-        // generate table definition with too many columns - over limit on every backend
-        // don't make that number ridiculously big. RequestObject won't make it...
-        for ($i = 1; $i <= 2100; $i++) {
-            $data['columns'][] = [
-                'name' => 'col' . $i,
-                'basetype' => 'BOOLEAN',
-            ];
-        }
-        try {
-            $this->_client->createTableDefinition(
-                $this->getTestBucketId(),
-                $data,
-            );
-            $this->fail('There were 2100 columns which should fail.');
-        } catch (ClientException $e) {
-            $this->assertEquals('storage.tables.validation.tooManyColumns', $e->getStringCode());
-        }
-    }
-
     /**
      * Tests: https://github.com/keboola/connection/issues/246
      */
---
+++ tests/Backend/CommonPart2/TablesDefinitionCommonTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Keboola\Test\Backend\CommonPart2;
+
+use Keboola\StorageApi\ClientException;
+use Keboola\Test\StorageApiTestCase;
+use Keboola\Csv\CsvFile;
+
+class TablesDefinitionCommonTest extends StorageApiTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $token = $this->_client->verifyToken();
+
+        if (!in_array('tables-definition', $token['owner']['features'])) {
+            $this->fail(sprintf('Tables definition feature is not enabled for project "%s"', $token['owner']['id']));
+        }
+
+        $this->initEmptyTestBucketsForParallelTests();
+    }
+
+    public function testTooManyColumnsCountFailed(): void
+    {
+        $this->skipTestForBackend([
+            self::BACKEND_REDSHIFT,
+        ], 'Backend does not support table-definition feature');
+        $this->skipTestForBackend([
+            self::BACKEND_EXASOL,
+            self::BACKEND_BIGQUERY,
+        ], 'Backend has no limit regarding number of columns');
+
+        $data = [
+            'name' => 'tooManyColumns',
+            'primaryKeysNames' => [],
+            'columns' => [],
+        ];
+        // generate table definition with too many columns - over limit on every backend
+        // don't make that number ridiculously big. RequestObject won't make it...
+        for ($i = 1; $i <= 2100; $i++) {
+            $data['columns'][] = [
+                'name' => 'col' . $i,
+                'basetype' => 'BOOLEAN',
+            ];
+        }
+        try {
+            $this->_client->createTableDefinition(
+                $this->getTestBucketId(),
+                $data,
+            );
+            $this->fail('There were 2100 columns which should fail.');
+        } catch (ClientException $e) {
+            $this->assertEquals('storage.tables.validation.tooManyColumns', $e->getStringCode());
+        }
+    }
+}
