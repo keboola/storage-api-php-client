b6fae6f82c2a985afcf6640b92202df30d0e0b72
Roman Bracinik <roman.bracinik@keboola.com>
2023-04-18 18:21:11 +0200

    Test errors in datapreview where filters types

--- tests/Backend/Snowflake/TableDefinitionOperationsTest.php
+++ tests/Backend/Snowflake/TableDefinitionOperationsTest.php
@@ -2,6 +2,7 @@
 
 namespace Keboola\Test\Backend\Snowflake;
 
+use Generator;
 use Keboola\Csv\CsvFile;
 use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Metadata;
@@ -837,5 +838,215 @@ class TableDefinitionOperationsTest extends StorageApiTestCase
                 'timestamp',
             ]);
         }
+    }
+    /**
+     * @dataProvider  filterProvider
+     */
+    public function testColumnTypesInTableDefinition(array $params, string $expectExceptionMessage): void
+    {
+        $bucketId = $this->getTestBucketId(self::STAGE_IN);
+
+        $tableDefinition = [
+            'name' => 'my-new-table-for_data_preview',
+            'primaryKeysNames' => [],
+            'columns' => [
+                [
+                    'name' => 'column_int',
+                    'definition' => [
+                        'type' => 'INT',
+                    ],
+                ],
+                [
+                    'name' => 'column_number',
+                    'definition' => [
+                        'type' => 'NUMBER',
+                    ],
+                ],
+                [
+                    'name' => 'column_float',
+                    'definition' => [
+                        'type' => 'FLOAT',
+                    ],
+                ],
+                [
+                    'name' => 'column_varchar',
+                    'definition' => [
+                        'type' => 'VARCHAR',
+                    ],
+                ],
+                [
+                    'name' => 'column_datetime',
+                    'definition' => [
+                        'type' => 'DATETIME',
+                    ],
+                ],
+                [
+                    'name' => 'column_date',
+                    'definition' => [
+                        'type' => 'DATE',
+                    ],
+                ],
+                [
+                    'name' => 'column_time',
+                    'definition' => [
+                        'type' => 'TIME',
+                    ],
+                ],
+                [
+                    'name' => 'column_timestamp',
+                    'definition' => [
+                        'type' => 'TIMESTAMP',
+                    ],
+                ],
+                [
+                    'name' => 'column_boolean',
+                    'definition' => [
+                        'type' => 'BOOLEAN',
+                    ],
+                ],
+            ],
+        ];
+
+        $csvFile = $this->createTempCsv();
+        $csvFile->writeRow([
+            'column_int',
+            'column_number',
+            'column_float',
+            'column_varchar',
+            'column_datetime',
+            'column_date',
+            'column_time',
+            'column_timestamp',
+            'column_boolean',
+        ]);
+        $csvFile->writeRow(
+            [
+                '1',
+                '3.14',
+                '3.14',
+                'roman',
+                '1989-08-31 00:00:00.000',
+                '1989-08-31',
+                '12:00:00.000',
+                '2023-04-18 12:34:56',
+                0,
+            ]
+        );
+
+        $tableId = $this->_client->createTableDefinition($bucketId, $tableDefinition);
+
+        $this->_client->writeTableAsync($tableId, $csvFile);
+
+        $this->expectException(ClientException::class);
+        $this->expectExceptionMessage($expectExceptionMessage);
+        /** @var array $data */
+        $this->_client->getTableDataPreview($tableId, array_merge(['format' => 'json'], $params));
+    }
+
+    public function filterProvider(): Generator
+    {
+        yield 'overflow int' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_int',
+                        'operator' => 'gt',
+                        'values' => ['999999999999999999999999999999999999999'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Numeric value \'999999999999999999999999999999999999999\' is not recognized, SQL state 22005 in SQLExecute',
+        ];
+
+        yield 'wrong int' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_int',
+                        'operator' => 'eq',
+                        'values' => ['aaa'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Numeric value \'aaa\' is not recognized, SQL state 22005 in SQLExecute',
+        ];
+
+        yield 'wrong float' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_float',
+                        'operator' => 'eq',
+                        'values' => ['aaa'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Numeric value \'aaa\' is not recognized, SQL state 22005 in SQLExecute',
+        ];
+
+        yield 'wrong datetime' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_datetime',
+                        'operator' => 'eq',
+                        'values' => ['2022-02-31'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Timestamp \'2022-02-31\' is not recognized, SQL state 22008 in SQLExecute',
+        ];
+
+        yield 'wrong boolean' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_boolean',
+                        'operator' => 'eq',
+                        'values' => ['222'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Boolean value \'222\' is not recognized, SQL state 22005 in SQLExecute',
+        ];
+
+        yield 'wrong date' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_date',
+                        'operator' => 'eq',
+                        'values' => ['12:00:00.000'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Date \'12:00:00.000\' is not recognized, SQL state 22008 in SQLExecute',
+        ];
+
+        yield 'wrong time' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_time',
+                        'operator' => 'eq',
+                        'values' => ['1989-08-31'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Time \'1989-08-31\' is not recognized, SQL state 22008 in SQLExecute',
+        ];
+
+        yield 'wrong timestamp' => [
+            [
+                'whereFilters' => [
+                    [
+                        'column' => 'column_timestamp',
+                        'operator' => 'eq',
+                        'values' => ['xxx'],
+                    ],
+                ],
+            ],
+            'odbc_execute(): SQL error: Timestamp \'xxx\' is not recognized, SQL state 22008 in SQLExecute',
+        ];
     }
 }
