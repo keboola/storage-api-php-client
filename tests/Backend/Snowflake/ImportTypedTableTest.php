dcd1c77bc26db6e2ae413588eb8f99913aab899c
zajca <zajca@zajca.cz>
2023-05-16 13:57:44 +0200

    add testLoadTypedTablesLengthOverflowError to test import on typed table when string will not fit in column

--- tests/Backend/Snowflake/ImportTypedTableTest.php
+++ tests/Backend/Snowflake/ImportTypedTableTest.php
@@ -309,4 +309,57 @@ class ImportTypedTableTest extends ParallelWorkspacesTestCase
             self::assertSame("Table import error: Load error: An exception occurred while executing a query: Numeric value 'male' is not recognized", $e->getMessage());
         }
     }
+
+    public function testLoadTypedTablesLengthOverflowError(): void
+    {
+        $fullLoadFile = __DIR__ . '/../../_data/users.csv';
+
+        $bucketId = $this->getTestBucketId(self::STAGE_IN);
+        $payload = [
+            'name' => 'users-types',
+            'primaryKeysNames' => ['id'],
+            'columns' => [
+                ['name' => 'id', 'definition' => ['type' => 'INT', 'nullable' => false]],
+                ['name' => 'name', 'definition' => ['type' => 'VARCHAR', 'length' => 1]],
+                ['name' => 'city', 'definition' => ['type' => 'VARCHAR']],
+                ['name' => 'sex', 'definition' => ['type' => 'VARCHAR']],
+            ],
+        ];
+        $runId = $this->_client->generateRunId();
+        $this->_client->setRunId($runId);
+
+        // create table
+        $tableId = $this->_client->createTableDefinition($bucketId, $payload);
+
+        // block until async events are processed, processing in order is not guaranteed but it should work most of time
+        $this->createAndWaitForEvent((new \Keboola\StorageApi\Event())->setComponent('dummy')->setMessage('dummy'));
+
+        try {
+            // try import data with wrong types with full load
+            $this->_client->writeTableAsync(
+                $tableId,
+                new CsvFile($fullLoadFile),
+                [
+                    'incremental' => false,
+                ]
+            );
+            $this->fail('Should throw ClientException');
+        } catch (ClientException $e) {
+            self::assertSame("Load error: An exception occurred while executing a query: String 'martin' cannot be inserted because it's bigger than column size", $e->getMessage());
+        }
+
+        try {
+            // try import data with wrong types with incremental load
+            $this->_client->writeTableAsync(
+                $tableId,
+                new CsvFile($fullLoadFile),
+                [
+                    'incremental' => true,
+                ]
+            );
+            $this->fail('Should throw ClientException');
+        } catch (ClientException $e) {
+            self::assertSame("Load error: An exception occurred while executing a query: String 'martin' cannot be inserted because it's bigger than column size", $e->getMessage());
+        }
+    }
 }
