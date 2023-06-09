28b8cf1f66bcf7c2ce6d6ebde67b1fcaffb4450c
Tomáš Fejfar <tomas.fejfar@gmail.com>
2023-06-09 16:42:59 +0200

    SOX-1 add method to create privileged token

--- src/Keboola/StorageApi/Options/TokenCreateOptions.php
+++ src/Keboola/StorageApi/Options/TokenCreateOptions.php
@@ -10,6 +10,8 @@ class TokenCreateOptions extends TokenAbstractOptions
     /** @var bool */
     private $canManageBuckets = false;
 
+    private bool $protectedDefaultBranchPrivileged = false;
+
     /**
      * @return int|null
      */
@@ -46,6 +48,17 @@ class TokenCreateOptions extends TokenAbstractOptions
         return $this;
     }
 
+    public function setProtectedDefaultBranchPrivileged(bool $isProtectedDefaultBranchPrivileged): self
+    {
+        $this->protectedDefaultBranchPrivileged = $isProtectedDefaultBranchPrivileged;
+        return $this;
+    }
+
+    public function isProtectedDefaultBranchPrivileged(): bool
+    {
+        return $this->protectedDefaultBranchPrivileged;
+    }
+
     /**
      * @param bool $forJson return structure for form-data (false) or for JSON (true)
      * @return array
@@ -60,6 +73,10 @@ class TokenCreateOptions extends TokenAbstractOptions
 
         if ($this->getExpiresIn() !== null) {
             $params['expiresIn'] = $this->getExpiresIn();
+        }
+
+        if ($this->isProtectedDefaultBranchPrivileged()) {
+            $params['protectedDefaultBranchPrivileged'] = $this->isProtectedDefaultBranchPrivileged();
         }
 
         return $params;
