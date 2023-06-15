24889e7818ed72f89a4e7db34e7c313b7ef1ab5b
zajca <zajca@zajca.cz>
2023-06-15 10:28:14 +0200

    add snowflake testTableLoadAsView

--- tests/Backend/Workspaces/WorkspacesSnowflakeTest.php
+++ tests/Backend/Workspaces/WorkspacesSnowflakeTest.php
@@ -3,11 +3,11 @@
 namespace Keboola\Test\Backend\Workspaces;
 
 use Keboola\Csv\CsvFile;
-use Keboola\Db\Import\Snowflake\Connection;
 use Keboola\StorageApi\ClientException;
 use Keboola\StorageApi\Workspaces;
 use Keboola\Test\Backend\WorkspaceConnectionTrait;
 use Keboola\Test\Backend\Workspaces\Backend\WorkspaceBackendFactory;
+use Throwable;
 
 class WorkspacesSnowflakeTest extends ParallelWorkspacesTestCase
 {
@@ -835,5 +835,236 @@ class WorkspacesSnowflakeTest extends ParallelWorkspacesTestCase
                 false,
             ],
         ];
+    }
+
+    public function testTableLoadAsView(): void
+    {
+        $currentToken = $this->_client->verifyToken();
+        self::assertArrayHasKey('owner', $currentToken);
+        if (!in_array('input-mapping-read-only-storage', $currentToken['owner']['features'])) {
+            self::fail(sprintf(
+                'Project "%s" id:"%s" is missing feature "input-mapping-read-only-storage"',
+                $currentToken['owner']['name'],
+                $currentToken['owner']['id']
+            ));
+        }
+
+        $bucketId = $this->getTestBucketId(self::STAGE_IN);
+
+        $workspaces = new Workspaces($this->workspaceSapiClient);
+        $workspace = $workspaces->createWorkspace([], true);
+        $backend = WorkspaceBackendFactory::createWorkspaceForSnowflakeDbal($workspace);
+        $importFile = __DIR__ . '/../../_data/languages.csv';
+        $tableId = $this->_client->createTableAsync(
+            $bucketId,
+            'languages',
+            new CsvFile($importFile)
+        );
+
+        $options = [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languages',
+                    'useView' => true,
+                ],
+            ],
+        ];
+
+        $workspaces->loadWorkspaceData($workspace['id'], $options);
+
+        $tableRef = $backend->getTableReflection('languages');
+        $viewRef = $backend->getViewReflection('languages');
+        // View definition should be available
+        self::assertStringStartsWith('CREATE VIEW', $viewRef->getViewDefinition());
+        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+        self::assertCount(5, $backend->fetchAll('languages'));
+
+        // test if view select fail after column add
+        $this->_client->addTableColumn($tableId, 'newGuy');
+        $tableRef = $backend->getTableReflection('languages');
+        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+        try {
+            $backend->fetchAll('languages');
+            $this->fail('Must throw exception view columns mismatch');
+        } catch (Throwable $e) {
+            $this->assertStringContainsString('declared 3 column(s), but view query produces 4 column(s).', $e->getMessage());
+        }
+
+        // test that doesn't work after column remove
+        $this->_client->deleteTableColumn($tableId, 'name');
+        $tableRef = $backend->getTableReflection('languages');
+        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+        try {
+            $backend->fetchAll('languages');
+            $this->fail('Must throw exception view columns mismatch');
+        } catch (Throwable $e) {
+            $this->assertStringContainsString('View columns mismatch with view definition for view \'languages\'', $e->getMessage());
+        }
+
+        // clear and create table again
+        $backend->dropViewIfExists('languages');
+        $this->_client->dropTable($tableId);
+        $tableId = $this->_client->createTableAsync(
+            $bucketId,
+            'languages',
+            new CsvFile($importFile)
+        );
+        $workspaces->loadWorkspaceData($workspace['id'], $options);
+
+        // test preserve load
+        $options = [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languages',
+                    'useView' => true,
+                ],
+            ],
+            'preserve' => true,
+        ];
+        try {
+            $workspaces->loadWorkspaceData($workspace['id'], $options);
+            self::fail('Must throw exception view exists');
+        } catch (ClientException $e) {
+            self::assertEquals('Table languages already exists in workspace', $e->getMessage());
+        }
+
+        // test preserve load with overwrite
+        $options = [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languages',
+                    'useView' => true,
+                    'overwrite' => true,
+                ],
+            ],
+            'preserve' => true,
+        ];
+        $workspaces->loadWorkspaceData($workspace['id'], $options);
+        $tableRef = $backend->getTableReflection('languages');
+        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+        self::assertCount(5, $backend->fetchAll('languages'));
+
+        // test workspace is cleared load works
+        $options = [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languages',
+                    'useView' => true,
+                ],
+            ],
+        ];
+        $workspaces->loadWorkspaceData($workspace['id'], $options);
+        $tableRef = $backend->getTableReflection('languages');
+        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+        self::assertCount(5, $backend->fetchAll('languages'));
+
+        // test workspace load incremental to view
+        $options = [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languages',
+                    'incremental' => true,
+                    'useView' => false,
+                ],
+            ],
+            'preserve' => true,
+        ];
+        try {
+            $workspaces->loadWorkspaceData($workspace['id'], $options);
+            self::fail('Incremental load to view cannot work.');
+        } catch (ClientException $e) {
+            // this is expected edge case, view has also _timestamp col
+            // which is ignored when validation incremental load
+            self::assertStringStartsWith('Some columns are missing in source table', $e->getMessage());
+        }
+
+        // do incremental load from file to source table
+        $this->_client->writeTableAsync(
+            $tableId,
+            new CsvFile($importFile),
+            ['incremental' => true]
+        );
+        // test view is still working
+        $tableRef = $backend->getTableReflection('languages');
+        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+        self::assertCount(10, $backend->fetchAll('languages'));
+
+        // load data from workspace to table
+        $workspace2 = $workspaces->createWorkspace([], true);
+        $options = [
+            'input' => [
+                [
+                    'source' => $tableId,
+                    'destination' => 'languages',
+                ],
+            ],
+        ];
+        $workspaces->loadWorkspaceData($workspace2['id'], $options);
+        $this->_client->writeTableAsyncDirect(
+            $tableId,
+            [
+                'dataWorkspaceId' => $workspace2['id'],
+                'dataObject' => 'languages',
+            ]
+        );
+        // test view is still working
+        $tableRef = $backend->getTableReflection('languages');
+        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+        self::assertCount(10, $backend->fetchAll('languages'));
+
+//        $fileId = $this->workspaceSapiClient->uploadFile(
+//            (new CsvFile($importFile))->getPathname(),
+//            (new FileUploadOptions())
+//                ->setNotify(false)
+//                ->setIsPublic(false)
+//                ->setCompress(true)
+//                ->setTags(['test-file-1'])
+//        );
+//        // load data from file workspace Not supported yet on S3 on ABS and not used in SNFLK
+//        $fileWorkspace = $workspaces->createWorkspace(
+//            [
+//                'backend' => 'abs',
+//            ],
+//            true
+//        );
+//        $options = [
+//            'input' => [
+//                [
+//                    'dataFileId' => $fileId,
+//                    'destination' => 'languages',
+//                ],
+//            ],
+//        ];
+//        $workspaces->loadWorkspaceData($fileWorkspace['id'], $options);
+//        $this->_client->writeTableAsyncDirect(
+//            $tableId,
+//            [
+//                'dataWorkspaceId' => $fileWorkspace['id'],
+//                'dataObject' => 'languages/',
+//            ]
+//        );
+//        // test view is still working
+//        $tableRef = $backend->getTableReflection('languages');
+//        self::assertEquals(['id', 'name', '_timestamp'], $tableRef->getColumnsNames());
+//        $backend->fetchAll('languages');
+//        self::assertCount(5, $backend->fetchAll('languages'));
+
+        // test drop table
+        $this->_client->dropTable($tableId);
+        $schemaRef = $backend->getSchemaReflection();
+        self::assertCount(0, $schemaRef->getTablesNames());
+        // view is still in workspace but not working
+        self::assertCount(1, $schemaRef->getViewsNames());
+        try {
+            $backend->fetchAll('languages');
+            $this->fail('View should not work after table drop');
+        } catch (Throwable $e) {
+            $this->assertStringContainsString('does not exist or not authorized', $e->getMessage());
+        }
     }
 }
