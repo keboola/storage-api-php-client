90863950d380ea7782ea777aa26785a0f63d4502
Tomáš Fejfar <tomas.fejfar@gmail.com>
2023-06-20 15:33:40 +0200

    SOX-66 Refactor setup to method to allow reusing - move setUpForProtectedDevBranch

--- tests/ClientProvider/TestSetupHelper.php
+++ tests/ClientProvider/TestSetupHelper.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Keboola\Test\ClientProvider;
 
+use Keboola\StorageApi\Client;
 use Keboola\StorageApi\DevBranches;
 
 class TestSetupHelper
@@ -23,5 +24,52 @@ class TestSetupHelper
         foreach ($branchesCreatedByThisTestMethod as $branch) {
             $devBranches->deleteBranch($branch['id']);
         }
+    }
+
+    /**
+     * @param ClientProvider $clientProvider
+     * @param ClientProvider::*_BRANCH $devBranchType
+     * @param string $userRole
+     * @return array{0: Client, 1: Client}
+     */
+    public function setUpForProtectedDevBranch(
+        ClientProvider $clientProvider,
+        string $devBranchType,
+        string $userRole
+    ): array {
+        $hasProjectProtectedDefaultbranch = in_array($userRole, ['reviewer', 'developer', 'production-manager']);
+
+        $client = $clientProvider->getDefaultClient();
+        if ($hasProjectProtectedDefaultbranch) {
+            // default branch is protected, we need privileged client for production cleanup
+            $client = $clientProvider->getDefaultClient(['token' => STORAGE_API_DEFAULT_BRANCH_TOKEN]);
+        }
+
+        if ($devBranchType === ClientProvider::DEFAULT_BRANCH && $userRole === 'production-manager') {
+            $testClient = $clientProvider->getDefaultClient(['token' => STORAGE_API_DEFAULT_BRANCH_TOKEN]);
+        } elseif ($devBranchType === ClientProvider::DEV_BRANCH && $userRole === 'developer') {
+            $branchName = $clientProvider->getDevBranchName();
+            // dev can create & delete branches in production
+            $devBranches = new DevBranches($clientProvider->getDefaultClient(['token' => STORAGE_API_DEVELOPER_TOKEN]));
+            $this->deleteBranchesByPrefix($devBranches, $branchName);
+            $branch = $devBranches->createBranch($branchName);
+
+            // branched client for dev
+            $testClient = $clientProvider->getBranchAwareClient($branch['id'], [
+                'token' => STORAGE_API_DEVELOPER_TOKEN,
+                'url' => STORAGE_API_URL,
+                'backoffMaxTries' => 1,
+                'jobPollRetryDelay' => function () {
+                    return 1;
+                },
+            ]);
+        } elseif ($devBranchType === ClientProvider::DEFAULT_BRANCH && $userRole === 'admin') {
+            // fallback for normal tests
+            $testClient = $clientProvider->createClientForCurrentTest();
+        } else {
+            throw new \Exception(sprintf('Unknown combination of devBranchType "%s" and userRole "%s"', $devBranchType, $userRole));
+        }
+
+        return [$client, $testClient];
     }
 }
