e3cde3614f465c4a63d27db3856e228256acb3e1
Martin Jandl <martin.jandl@gmail.com>
2024-03-18 12:50:54 +0100

    Check expected exception message

--- tests/Backend/CommonPart1/DeleteRowsTest.php
+++ tests/Backend/CommonPart1/DeleteRowsTest.php
@@ -88,7 +88,8 @@ class DeleteRowsTest extends StorageApiTestCase
             ]);
             $this->fail('Exception should be thrown');
         } catch (\Keboola\StorageApi\ClientException $e) {
-            $this->assertEquals('validation.failed', $e->getStringCode());
+            $this->assertSame('validation.failed', $e->getStringCode());
+            $this->assertSame("Invalid request:\n - whereColumn: \"To use whereColumn specify whereValues\"", $e->getMessage());
         }
     }
 
