f93d3058ed7470147f10409e603fc0ac9e654a8f
Jiri Semmler <jirka.semmler@gmail.com>
2024-03-19 15:17:45 +0100

    codestyle

--- tests/Backend/CommonPart1/DeleteTableTest.php
+++ tests/Backend/CommonPart1/DeleteTableTest.php
@@ -3,7 +3,6 @@
 namespace Keboola\Test\Backend\CommonPart1;
 
 use Generator;
-use Keboola\Csv\InvalidArgumentException;
 use Keboola\Test\StorageApiTestCase;
 use Keboola\Csv\CsvFile;
 
@@ -20,14 +19,14 @@ class DeleteTableTest extends StorageApiTestCase
      */
     public function testTableDelete(bool $async): void
     {
-        $table1Id = $this->_client->createTableAsync($this->getTestBucketId(self::STAGE_IN), 'languages', new CsvFile(__DIR__ . '/../../_data/languages.csv'));
-        $table2Id = $this->_client->createTableAsync($this->getTestBucketId(self::STAGE_IN), 'languages_2', new CsvFile(__DIR__ . '/../../_data/languages.csv'));
-        $tables = $this->_client->listTables($this->getTestBucketId(self::STAGE_IN));
+        $table1Id = $this->_client->createTableAsync($this->getTestBucketId(), 'languages', new CsvFile(__DIR__ . '/../../_data/languages.csv'));
+        $table2Id = $this->_client->createTableAsync($this->getTestBucketId(), 'languages_2', new CsvFile(__DIR__ . '/../../_data/languages.csv'));
+        $tables = $this->_client->listTables($this->getTestBucketId());
 
         $this->assertCount(2, $tables);
         $this->_client->dropTable($table1Id, ['async' => $async]);
 
-        $tables = $this->_client->listTables($this->getTestBucketId(self::STAGE_IN));
+        $tables = $this->_client->listTables($this->getTestBucketId());
         $this->assertCount(1, $tables);
 
         $table = reset($tables);
