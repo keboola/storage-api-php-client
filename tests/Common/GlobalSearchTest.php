dc0b2cb87dd43fb210ed627e5bc1b3f8e7519f5e
Vojta Biberle <vojtech.biberle@gmail.com>
2024-06-11 10:59:18 +0200

    CT-1445 - docs & test

--- tests/Common/GlobalSearchTest.php
+++ tests/Common/GlobalSearchTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Common;
 
+use Keboola\Csv\CsvFile;
 use Keboola\StorageApi\Client;
+use Keboola\StorageApi\Components;
 use Keboola\StorageApi\Options\BucketUpdateOptions;
+use Keboola\StorageApi\Options\Components\Configuration;
 use Keboola\StorageApi\Options\Components\ConfigurationRow;
 use Keboola\Test\ClientProvider\ClientProvider;
 use Keboola\Test\StorageApiTestCase;
@@ -36,9 +39,9 @@ class GlobalSearchTest extends StorageApiTestCase
     {
         $name = 'main-'.$this->generateDescriptionForTestObject();
         $hashedName = sha1($name);
-        $components = new \Keboola\StorageApi\Components($this->client);
+        $components = new Components($this->client);
 
-        $configuration = new \Keboola\StorageApi\Options\Components\Configuration();
+        $configuration = new Configuration();
         $configuration
             ->setComponentId('wr-db')
             ->setConfigurationId('main-1')
@@ -80,9 +83,9 @@ class GlobalSearchTest extends StorageApiTestCase
     {
         $rowName = 'main-1'.$this->generateDescriptionForTestObject();
         $hashedRowName = sha1($rowName);
-        $components = new \Keboola\StorageApi\Components($this->client);
+        $components = new Components($this->client);
 
-        $configuration = new \Keboola\StorageApi\Options\Components\Configuration();
+        $configuration = new Configuration();
         $configuration
             ->setComponentId('wr-db')
             ->setConfigurationId('main-1')
@@ -165,7 +168,7 @@ class GlobalSearchTest extends StorageApiTestCase
 
         $tableName = 'table'.$this->generateDescriptionForTestObject();
         $hashedTableName = sha1($tableName);
-        $this->_client->createTableAsync($testBucketId, $hashedTableName, new \Keboola\Csv\CsvFile(__DIR__ . '/../_data/languages.csv'));
+        $this->_client->createTableAsync($testBucketId, $hashedTableName, new CsvFile(__DIR__ . '/../_data/languages.csv'));
         $apiCall = fn() => $this->client->globalSearch($hashedTableName);
         $assertCallback = function ($searchResult) {
             $this->assertSame(1, $searchResult['all']);
@@ -213,5 +216,104 @@ class GlobalSearchTest extends StorageApiTestCase
             $this->assertSame(0, $searchResult['all']);
         };
         $this->retryWithCallback($apiCall, $assertCallback);
+    }
+
+    public function testGlobalSearchTogether(): void
+    {
+        $configurationName = 'main-'.$this->generateDescriptionForTestObject();
+        $configurationHashedName = sha1($configurationName);
+        $components = new Components($this->client);
+
+        $configuration1 = new Configuration();
+        $configuration1
+            ->setComponentId('wr-db')
+            ->setConfigurationId('main-1')
+            ->setName($configurationHashedName)
+            ->setDescription('description of first configuration');
+
+        $components->addConfiguration($configuration1);
+
+        $rowName = 'main-1'.$this->generateDescriptionForTestObject();
+        $hashedRowName = sha1($rowName);
+        $components = new Components($this->client);
+
+        $configuration2 = new Configuration();
+        $configurationName2 = 'main2-'.$this->generateDescriptionForTestObject();
+        $configurationHashedName2 = sha1($configurationName2);
+        $configuration2
+            ->setComponentId('wr-db')
+            ->setConfigurationId('main-2')
+            ->setName($configurationHashedName2.' 2 HR')
+            ->setDescription('description of second configuration');
+
+        $components->addConfiguration($configuration2);
+
+        $components->addConfigurationRow(
+            (new ConfigurationRow($configuration2))
+                ->setName($hashedRowName)
+                ->setRowId('main-1-row-1'),
+        );
+
+        $bucketName1 = 'main'.$this->generateDescriptionForTestObject();
+        $hashedBucketName1 = sha1($bucketName1);
+
+        $testBucketId1 = 'in.c-' . $hashedBucketName1;
+        $this->dropBucketIfExists($this->_client, $testBucketId1, true);
+        $this->_client->createBucket($hashedBucketName1, 'in', $bucketName1);
+
+        $bucketName2 = 'main'.$this->generateDescriptionForTestObject();
+        $hashedBucketName2 = sha1($bucketName2);
+
+        $testBucketId2 = 'in.c-' . $hashedBucketName2;
+        $this->dropBucketIfExists($this->_client, $testBucketId2, true);
+        $this->_client->createBucket($hashedBucketName2, 'in', $bucketName2);
+
+        $tableName1 = 'table'.$this->generateDescriptionForTestObject();
+        $hashedTableName1 = sha1($tableName1);
+        $this->_client->createTableAsync($testBucketId2, $hashedTableName1, new CsvFile(__DIR__ . '/../_data/languages.csv'));
+
+        $apiCall1 = fn() => $this->client->globalSearch($configurationHashedName);
+        $assertCallback1 = function ($searchResult) use ($configurationHashedName) {
+            $this->assertSame(1, $searchResult['all']);
+            $this->assertArrayHasKey('id', $searchResult['items'][0]);
+            $this->assertArrayHasKey('type', $searchResult['items'][0]);
+            $this->assertEquals('configuration', $searchResult['items'][0]['type']);
+            $this->assertArrayHasKey('name', $searchResult['items'][0]);
+            $this->assertEquals($searchResult['items'][0]['name'], $configurationHashedName);
+        };
+        $this->retryWithCallback($apiCall1, $assertCallback1);
+
+        $apiCall2 = fn() => $this->client->globalSearch($configurationHashedName2);
+        $assertCallback2 = function ($searchResult) use ($configurationHashedName2) {
+            $this->assertSame(1, $searchResult['all']);
+            $this->assertArrayHasKey('id', $searchResult['items'][0]);
+            $this->assertArrayHasKey('type', $searchResult['items'][0]);
+            $this->assertEquals('configuration', $searchResult['items'][0]['type']);
+            $this->assertArrayHasKey('name', $searchResult['items'][0]);
+            $this->assertStringStartsWith($configurationHashedName2, $searchResult['items'][0]['name']);
+        };
+        $this->retryWithCallback($apiCall2, $assertCallback2);
+
+        $apiCall3 = fn() => $this->client->globalSearch($hashedBucketName1);
+        $assertCallback3 = function ($searchResult) use ($hashedBucketName1) {
+            $this->assertSame(1, $searchResult['all']);
+            $this->assertArrayHasKey('id', $searchResult['items'][0]);
+            $this->assertArrayHasKey('type', $searchResult['items'][0]);
+            $this->assertEquals('bucket', $searchResult['items'][0]['type']);
+            $this->assertArrayHasKey('name', $searchResult['items'][0]);
+            $this->assertStringStartsWith($hashedBucketName1, $searchResult['items'][0]['name']);
+        };
+        $this->retryWithCallback($apiCall3, $assertCallback3);
+
+        $apiCall4 = fn() => $this->client->globalSearch($hashedTableName1);
+        $assertCallback4 = function ($searchResult) use ($hashedTableName1) {
+            $this->assertSame(1, $searchResult['all']);
+            $this->assertArrayHasKey('id', $searchResult['items'][0]);
+            $this->assertArrayHasKey('type', $searchResult['items'][0]);
+            $this->assertEquals('table', $searchResult['items'][0]['type']);
+            $this->assertArrayHasKey('name', $searchResult['items'][0]);
+            $this->assertStringStartsWith($hashedTableName1, $searchResult['items'][0]['name']);
+        };
+        $this->retryWithCallback($apiCall4, $assertCallback4);
     }
 }
