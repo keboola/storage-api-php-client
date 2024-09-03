7645e10a8692d017c58534a8a6065a2a962552ef
Roman Bracinik <roman.bracinik@keboola.com>
2024-09-03 13:12:56 +0200

    fix phpstan and improve absUrlParser
    phpstan starts failing so I fix it and improve class a bit and add test

--- tests/Downloader/AbsUrlParserTest.php
+++ tests/Downloader/AbsUrlParserTest.php
@@ -2,6 +2,7 @@
 
 namespace Keboola\Test\Downloader;
 
+use InvalidArgumentException;
 use Keboola\StorageApi\Downloader\AbsUrlParser;
 use PHPUnit\Framework\TestCase;
 
@@ -27,5 +28,12 @@ class AbsUrlParserTest extends TestCase
         $this->assertEquals('myaccount', $account);
         $this->assertEquals('mycontainer', $container);
         $this->assertEquals('myblob', $file);
+    }
+
+    public function testException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The provided URL does not match the expected format.');
+        AbsUrlParser::parseAbsUrl('invalid-url');
     }
 }
