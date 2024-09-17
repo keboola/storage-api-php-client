da59144fc0c04e7b34d9026430d1886ef3988bb0
Vojta Biberle <vojtech.biberle@gmail.com>
2024-09-17 15:40:54 +0200

    CT-1721 add new network-policy tests

---
+++ tests/Backend/NetworkPolicies/SnowflakeNetworkPoliciesTest.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\Test\Backend\NetworkPolicies;
+
+use Keboola\Db\Import\Exception;
+use Keboola\StorageApi\Workspaces;
+use Keboola\Test\Backend\Workspaces\Backend\WorkspaceBackendFactory;
+
+class SnowflakeNetworkPoliciesTest extends NetworkPoliciesTestCase
+{
+    public function testAccessWithoutSystemNetworkPolicy(): void
+    {
+        $systemNetworkPolicyName = $this->defaultNetworkPolicyName();
+        $this->assertNetworkPolicyNotExists($systemNetworkPolicyName);
+
+        $workspaces = new Workspaces($this->_client);
+        $workspace = $workspaces->createWorkspace(['backend' => 'snowflake'], true);
+
+        $this->assertDontHaveNetworkPolicyEnabled($workspace['connection']['user'], $systemNetworkPolicyName);
+
+        $workspaceBackend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
+
+        // expect no exception so workspace have still access to Snowflake
+        $workspaceBackend->createTable('NP_TEST_TABLE', ['ID' => 'INT', 'LASTNAME' => 'VARCHAR(255)']);
+        $workspaceBackend->dropTable('NP_TEST_TABLE');
+
+        $workspace2 = $workspaces->createWorkspace(['backend' => 'snowflake', 'networkPolicy' => 'system'], true);
+
+        $this->assertDontHaveNetworkPolicyEnabled($workspace2['connection']['user'], $systemNetworkPolicyName);
+
+        $workspace2Backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace2);
+
+        // expect no exception so workspace have still access to Snowflake
+        $workspace2Backend->createTable('NP_TEST_TABLE', ['ID' => 'INT', 'LASTNAME' => 'VARCHAR(255)']);
+        $workspace2Backend->dropTable('NP_TEST_TABLE');
+
+        $workspaces->deleteWorkspace($workspace['id']);
+        $workspaces->deleteWorkspace($workspace2['id']);
+    }
+
+    public function testAccessWithSystemNetworkPolicy(): void
+    {
+        $workspaces = new Workspaces($this->_client);
+        $workspace0 = $workspaces->createWorkspace(['backend' => 'snowflake'], true);
+        $databaseName = $workspace0['connection']['database'];
+
+        $systemNetworkPolicyName = $this->defaultNetworkPolicyName();
+
+        if ($this->networkPolicyExists($systemNetworkPolicyName)) {
+            $this->dropNetworkPolicy($systemNetworkPolicyName);
+        }
+        $this->createNetworkPolicyWithMyPublicIp($databaseName, $systemNetworkPolicyName);
+
+        $workspaces = new Workspaces($this->_client);
+        $workspace1 = $workspaces->createWorkspace(['backend' => 'snowflake'], true);
+
+        $this->assertDontHaveNetworkPolicyEnabled($workspace1['connection']['user'], $systemNetworkPolicyName);
+
+        $workspace1Backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace1);
+
+        // expect no exception so workspace have still access to Snowflake
+        $workspace1Backend->createTable('NP_TEST_TABLE', ['ID' => 'INT', 'LASTNAME' => 'VARCHAR(255)']);
+        $workspace1Backend->dropTable('NP_TEST_TABLE');
+
+        $workspace2 = $workspaces->createWorkspace(['backend' => 'snowflake', 'networkPolicy' => 'system'], false);
+
+        $this->assertHaveNetworkPolicyEnabled($workspace2['connection']['user'], $systemNetworkPolicyName);
+
+        $workspace2Backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace2);
+
+        // expect no exception so workspace have still access to Snowflake
+        $workspace2Backend->createTable('NP_TEST_TABLE', ['ID' => 'INT', 'LASTNAME' => 'VARCHAR(255)']);
+        $workspace2Backend->dropTable('NP_TEST_TABLE');
+
+        $workspaces->deleteWorkspace($workspace0['id']);
+        $workspaces->deleteWorkspace($workspace1['id']);
+        $workspaces->deleteWorkspace($workspace2['id']);
+
+        $this->dropNetworkPolicy($systemNetworkPolicyName);
+    }
+
+    public function testAccessWithPrivateIpInNetworkPolicy(): void
+    {
+        $workspaces = new Workspaces($this->_client);
+        $workspace0 = $workspaces->createWorkspace(['backend' => 'snowflake'], true);
+        $databaseName = $workspace0['connection']['database'];
+
+        $systemNetworkPolicyName = $this->defaultNetworkPolicyName();
+
+        if ($this->networkPolicyExists($systemNetworkPolicyName)) {
+            $this->dropNetworkPolicy($systemNetworkPolicyName);
+        }
+        $this->createNetworkPolicyWithPrivateIp($databaseName, $systemNetworkPolicyName);
+
+        $workspaces = new Workspaces($this->_client);
+        $workspace1 = $workspaces->createWorkspace(['backend' => 'snowflake'], true);
+
+        $this->assertDontHaveNetworkPolicyEnabled($workspace1['connection']['user'], $systemNetworkPolicyName);
+
+        $workspace1Backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace1);
+
+        // expect no exception so workspace have still access to Snowflake
+        $workspace1Backend->createTable('NP_TEST_TABLE', ['ID' => 'INT', 'LASTNAME' => 'VARCHAR(255)']);
+        $workspace1Backend->dropTable('NP_TEST_TABLE');
+
+        $workspace2 = $workspaces->createWorkspace(['backend' => 'snowflake', 'networkPolicy' => 'system'], false);
+
+        $this->assertHaveNetworkPolicyEnabled($workspace2['connection']['user'], $systemNetworkPolicyName);
+
+        try {
+            WorkspaceBackendFactory::createWorkspaceBackend($workspace2);
+            $this->fail('This should fail on "not allowed access to Snowflake"');
+        } catch (Exception $exception) {
+            $this->assertTrue(
+                str_contains('is not allowed to access Snowflake', $exception->getMessage()),
+                sprintf('User %s have still access to Snowflake', $workspace2['connection']['user']),
+            );
+        }
+
+        $workspaces->deleteWorkspace($workspace0['id']);
+        $workspaces->deleteWorkspace($workspace1['id']);
+        $workspaces->deleteWorkspace($workspace2['id']);
+
+        $this->dropNetworkPolicy($systemNetworkPolicyName);
+    }
+}
--- tests/Utils/ConnectionUtils.php
+++ tests/Utils/ConnectionUtils.php
@@ -9,20 +9,33 @@ use Keboola\TableBackendUtils\Connection\Snowflake\SnowflakeConnectionFactory;
 
 trait ConnectionUtils
 {
+    private static ?Connection $connection = null;
+
     public function ensureSnowflakeConnection(): Connection
     {
-        $host = getenv('SNOWFLAKE_HOST');
-        assert($host !== false, 'SNOWFLAKE_HOST env var is not set');
-        $user = getenv('SNOWFLAKE_USER');
-        assert($user !== false, 'SNOWFLAKE_USER env var is not set');
-        $pass = getenv('SNOWFLAKE_PASSWORD');
-        assert($pass !== false, 'SNOWFLAKE_PASSWORD env var is not set');
-        $warehouse = getenv('SNOWFLAKE_WAREHOUSE');
-        $params = [];
-        if ($warehouse !== false) {
-            $params['warehouse'] = $warehouse;
+        if (self::$connection === null) {
+            $host = getenv('SNOWFLAKE_HOST');
+            assert($host !== false, 'SNOWFLAKE_HOST env var is not set');
+            $user = getenv('SNOWFLAKE_USER');
+            assert($user !== false, 'SNOWFLAKE_USER env var is not set');
+            $pass = getenv('SNOWFLAKE_PASSWORD');
+            assert($pass !== false, 'SNOWFLAKE_PASSWORD env var is not set');
+            $warehouse = getenv('SNOWFLAKE_WAREHOUSE');
+            $params = [];
+            if ($warehouse !== false) {
+                $params['warehouse'] = $warehouse;
+            }
+
+            self::$connection = SnowflakeConnectionFactory::getConnection($host, $user, $pass, $params);
         }
 
-        return SnowflakeConnectionFactory::getConnection($host, $user, $pass, $params);
+        return self::$connection;
+    }
+
+    public function getSnowflakeUser(): string
+    {
+        $user = getenv('SNOWFLAKE_USER');
+        assert($user !== false, 'SNOWFLAKE_USER env var is not set');
+        return $user;
     }
 }
