3084169b7ab315b58660c120ab3ee71c8b403992
Vojta Biberle <vojtech.biberle@gmail.com>
2024-09-18 00:23:20 +0200

    CT-1721 fix phpstan

--- tests/Utils/IpUtils.php
+++ tests/Utils/IpUtils.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Keboola\Test\Utils;
 
+use RuntimeException;
+
 trait IpUtils
 {
     private static ?string $publicIp = null;
@@ -15,7 +17,11 @@ trait IpUtils
             curl_setopt($curlSession, CURLOPT_URL, 'http://ifconfig.me/ip');
             curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
 
-            self::$publicIp = curl_exec($curlSession);
+            $response = curl_exec($curlSession);
+            if ($response === false || !is_string($response)) {
+                throw new RuntimeException(sprintf('Curl error: %s', curl_error($curlSession)));
+            }
+            self::$publicIp = $response;
             curl_close($curlSession);
         }
 
