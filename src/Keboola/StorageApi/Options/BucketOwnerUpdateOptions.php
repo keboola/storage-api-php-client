b5f3c29de5cfcea2189ba986d29348184356c123
Vojta Biberle <vojtech.biberle@gmail.com>
2024-11-19 20:20:23 +0100

    CT-1846 - new client methods

---
+++ src/Keboola/StorageApi/Options/BucketOwnerUpdateOptions.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Keboola\StorageApi\Options;
+
+use InvalidArgumentException;
+use const JSON_THROW_ON_ERROR;
+
+class BucketOwnerUpdateOptions
+{
+    private ?int $id;
+    private ?string $email;
+
+    /**
+     * @param int|null $id
+     * @param string|null $email
+     */
+    public function __construct(?int $id = null, ?string $email = null)
+    {
+        $this->id = $id;
+        $this->email = $email;
+
+        if ($this->id !== null && $this->email !== null) {
+            throw new InvalidArgumentException('Only one attribute is accepted. Fill "id" or "email" not both.');
+        }
+
+        if ($this->id === null && $this->email === null) {
+            throw new InvalidArgumentException('At least one attribute must be specified. Fill "id" or "email".');
+        }
+    }
+
+    public function toArray(): array
+    {
+        $payload = [];
+        if ($this->id !== null) {
+            $payload['id'] = $this->id;
+        }
+        if ($this->email !== null) {
+            $payload['email'] = $this->email;
+        }
+        return $payload;
+    }
+}
