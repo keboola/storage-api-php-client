d8441ad49422034716ce1b00a9c1022ed261cbec
zajca <zajca@zajca.cz>
2024-12-16 12:15:56 +0100

    increase retry on table exists BQ

--- tests/Backend/Workspaces/Backend/BigqueryWorkspaceBackend.php
+++ tests/Backend/Workspaces/Backend/BigqueryWorkspaceBackend.php
@@ -17,6 +17,7 @@ use Keboola\TableBackendUtils\Table\Bigquery\BigqueryTableQueryBuilder;
 use Keboola\TableBackendUtils\Table\Bigquery\BigqueryTableReflection;
 use Keboola\TableBackendUtils\View\ViewReflectionInterface;
 use PDO;
+use Retry\Policy\SimpleRetryPolicy;
 use Retry\RetryProxy;
 
 class BigqueryWorkspaceBackend implements WorkspaceBackend
@@ -219,7 +220,7 @@ class BigqueryWorkspaceBackend implements WorkspaceBackend
         $dataset = $this->bqClient->dataset($schema);
         $table = $dataset->table($table);
 
-        $exists = (new RetryProxy())->call(function () use ($table) {
+        $exists = (new RetryProxy(new SimpleRetryPolicy(10)))->call(function () use ($table) {
             return $table->exists();
         });
         assert(is_bool($exists));
