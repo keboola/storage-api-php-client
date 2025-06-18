779695b20161bcd4c486654dcce8b4d84438dc1e
zajca <zajca@zajca.cz>
2025-06-18 11:07:59 +0200

    DMD-175 add testUnloadFromWSToTypedTableCTAS case for feature ctas-om

--- tests/Backend/Snowflake/TypedTableInWorkspaceTest.php
+++ tests/Backend/Snowflake/TypedTableInWorkspaceTest.php
@@ -119,6 +119,224 @@ class TypedTableInWorkspaceTest extends ParallelWorkspacesTestCase
         $this->unloadAndAssert($workspace['id'], $tableId);
     }
 
+    public function testUnloadFromWSToTypedTableCTAS(): void
+    {
+        if (!in_array('ctas-om', $this->_client->verifyToken()['owner']['features'], true)) {
+            $this->markTestSkipped(
+                'CTAS is not enabled for this project, skipping test.',
+            );
+        }
+        // create workspace and source table in workspace
+        $workspace = $this->initTestWorkspace();
+
+        $tableId = 'Languages3';
+
+        $backend = WorkspaceBackendFactory::createWorkspaceBackend($workspace);
+        $backend->dropTableIfExists($tableId);
+
+        $connection = $workspace['connection'];
+
+        $db = $backend->getDb();
+
+        $quotedTableId = sprintf(
+            '"%s"."%s"',
+            $connection['schema'],
+            $tableId,
+        );
+
+        $sql = sprintf(
+            '
+            CREATE TABLE %s (
+                "id" FLOAT NOT NULL,
+                "name" VARCHAR(5000)
+            )
+        ',
+            $quotedTableId,
+        );
+        $db->query($sql);
+        $db->query(sprintf("INSERT INTO %s VALUES (1, 'john');", $quotedTableId));
+        $db->query(sprintf("INSERT INTO %s VALUES (2, 'does');", $quotedTableId));
+
+        $db->query(sprintf('DROP TABLE %s', $quotedTableId));
+        // exact length is required
+        $sql = sprintf(
+            '
+            CREATE TABLE %s (
+                "id" INT NOT NULL,
+                "name" VARCHAR(16777216),
+                PRIMARY KEY ("id")
+            )
+        ',
+            $quotedTableId,
+        );
+        $db->query($sql);
+        $db->query(sprintf("INSERT INTO %s VALUES (1, 'john');", $quotedTableId));
+        $db->query(sprintf("INSERT INTO %s VALUES (2, 'does');", $quotedTableId));
+
+        $this->unloadAndAssert($workspace['id'], $tableId);
+
+        // test unload from workspace with _timestamp column exist
+        $db->query(sprintf('DROP TABLE %s', $quotedTableId));
+        $sql = sprintf(
+            '
+            CREATE TABLE %s (
+                "id" INT NOT NULL,
+                "name" VARCHAR(16777216),
+                PRIMARY KEY ("id")
+            )
+        ',
+            $quotedTableId,
+        );
+        $db->query($sql);
+        $db->query(sprintf("INSERT INTO %s VALUES (1, 'john');", $quotedTableId));
+        $db->query(sprintf("INSERT INTO %s VALUES (2, 'does');", $quotedTableId));
+        // snowflake does not enforce primary key so this is allowed
+        // normally such value would be deduplicated in storage
+        // but ctas-om does not deduplicate values
+        $db->query(sprintf("INSERT INTO %s VALUES (2, 'doesToo');", $quotedTableId));
+
+        // do full load
+        $this->_client->writeTableAsyncDirect($this->tableId, [
+            'dataWorkspaceId' => $workspace['id'],
+            'dataTableName' => $tableId,
+        ]);
+
+        $expected = [
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '1',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'john',
+                    'isTruncated' => false,
+                ],
+            ],
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '2',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'does',
+                    'isTruncated' => false,
+                ],
+            ],
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '2',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'doesToo',
+                    'isTruncated' => false,
+                ],
+            ],
+        ];
+
+        /** @var array $data */
+        $data = $this->_client->getTableDataPreview($this->tableId, [
+            'format' => 'json',
+        ]);
+        self::assertEquals($expected, $data['rows']);
+
+        // do incremental load
+        // incremental load also does not deduplicate values
+        // this will cause that all values are present twice
+        $this->_client->writeTableAsyncDirect($this->tableId, [
+            'dataWorkspaceId' => $workspace['id'],
+            'dataTableName' => $tableId,
+            'incremental' => true,
+        ]);
+
+        $expected = [
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '1',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'john',
+                    'isTruncated' => false,
+                ],
+            ],
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '2',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'does',
+                    'isTruncated' => false,
+                ],
+            ],
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '2',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'doesToo',
+                    'isTruncated' => false,
+                ],
+            ],
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '1',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'john',
+                    'isTruncated' => false,
+                ],
+            ],
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '2',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'does',
+                    'isTruncated' => false,
+                ],
+            ],
+            [
+                [
+                    'columnName' => 'id',
+                    'value' => '2',
+                    'isTruncated' => false,
+                ],
+                [
+                    'columnName' => 'name',
+                    'value' => 'doesToo',
+                    'isTruncated' => false,
+                ],
+            ],
+        ];
+
+        /** @var array $data */
+        $data = $this->_client->getTableDataPreview($this->tableId, [
+            'format' => 'json',
+        ]);
+        self::assertEquals($expected, $data['rows']);
+    }
+
     private function unloadAndAssert(int $id, string $tableId): void
     {
         // should be OK tables types are matching
@@ -152,6 +370,7 @@ class TypedTableInWorkspaceTest extends ParallelWorkspacesTestCase
                     'isTruncated' => false,
                 ],
             ],
+
         ];
 
         /** @var array $data */
